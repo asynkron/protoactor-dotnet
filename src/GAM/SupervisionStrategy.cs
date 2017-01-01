bdc962bdddbc3925025820559065ee817b957793
rogeralsing <rogeralsing@gmail.com>
2017-01-01 17:16:18 +0100

    .NET implementation

---
+++ src/GAM/SupervisionStrategy.cs
@@ -0,0 +1,12 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="SupervisionStrategy.cs" company="Asynkron HB">
+//      Copyright (C) 2015-2016 Asynkron HB All rights reserved
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace GAM
+{
+    public class SupervisionStrategy
+    {
+    }
+}
