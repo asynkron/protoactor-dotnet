320088b7146da1f09c72786e93f9a1830cc2290d
Christian Palmstierna <christian.palmstierna@gmail.com>
2017-01-16 22:16:23 +0100

    Receive pipeline

--- tests/Proto.Actor.Tests/MQMCQueueTests.cs
+++ tests/Proto.Actor.Tests/MQMCQueueTests.cs
@@ -6,7 +6,7 @@
 
 using Xunit;
 
-namespace Proto.Actor.Tests
+namespace Proto.Tests
 {
     public class MQMCQueueTests
     {
---
+++ tests/Proto.Actor.Tests/ReceivePluginTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Proto.Tests
+{
+    public class ReceivePluginTests
+    {
+        [Fact]
+        public void Given_ReceivePlugins_Should_Call_In_Order_Then_Actor()
+        {
+            var logs = new List<int>();
+            var testMailbox = new ActorFixture.TestMailbox();
+            var actor = Actor.FromFunc(c =>
+            {
+                switch(c.Message)
+                {
+                    case string s:
+                        logs.Add(2);
+                        break;
+                }
+                return Actor.Done;
+            })
+            .WithReceivers(
+                async c =>
+                {
+                    switch (c.Message)
+                    {
+                        case string s:
+                            logs.Add(0);
+                            break;
+                    }
+                    await c.NextAsync();
+                },
+                async c =>
+                {
+                    switch (c.Message)
+                    {
+                        case string s:
+                            logs.Add(1);
+                            break;
+                    }
+                    await c.NextAsync();
+                })
+            .WithMailbox(() => testMailbox);
+            var pid = Actor.Spawn(actor);
+
+            pid.Tell("");
+
+            Assert.Equal(3, logs.Count);
+            Assert.Equal(0, logs[0]);
+            Assert.Equal(1, logs[1]);
+            Assert.Equal(2, logs[2]);
+        }
+    }
+
+    static class ActorFixture
+    {
+        public class TestMailbox : IMailbox
+        {
+            private IMessageInvoker _invoker;
+            private IDispatcher _dispatcher;
+
+            public void PostUserMessage(object msg)
+            {
+                _invoker.InvokeUserMessageAsync(msg).Wait();
+            }
+
+            public void PostSystemMessage(SystemMessage sys)
+            {
+                _invoker.InvokeSystemMessage(sys);
+            }
+
+            public void RegisterHandlers(IMessageInvoker invoker, IDispatcher dispatcher)
+            {
+                _invoker = invoker;
+            }
+        }
+    }
+}
