8f789be1f6f3345896f065da9da8ffd2e90bca65
prem <jasprem1987@gmail.com>
2017-02-07 23:31:21 +0100

    Add Proto contract for message package

--- examples/Chat/Server/Program.cs
+++ examples/Chat/Server/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        var props = Actor.FromFunc();
+        var props = Actor.FromFunc(ctx =>
+        {
+            var msg = ctx.Message;
+            switch (msg)
+            {
+                
+            }
+            return Actor.Done;
+        });
     }
 }
