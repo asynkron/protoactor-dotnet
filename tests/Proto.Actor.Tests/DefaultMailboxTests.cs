91f2c5968f659d3c5817006a333bc75a6d9f71a2
Jêrome Rx <jerome@rouaix.eu>
2017-02-15 10:45:41 +0100

    BoundedMailboxQueue, UnboundedMailboxQueue some tests

---
+++ tests/Proto.Actor.Tests/DefaultMailboxTests.cs
@@ -0,0 +1,15 @@
+﻿using Proto.Mailbox;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proto.Tests
+{
+    public class DefaultMailboxTests
+    {
+        public void test()
+        {
+            var test = new DefaultMailbox(new UnboundedMailboxQueue(), new UnboundedMailboxQueue());
+        }
+    }
+}
