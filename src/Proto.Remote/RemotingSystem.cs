7ae521064474303e18b1cbb08d8f454e44ae0f16
rogeralsing <rogeralsing@gmail.com>
2017-03-17 20:00:27 +0100

    Implemented remote deployment

--- src/Proto.Remote/RemotingSystem.cs
+++ src/Proto.Remote/RemotingSystem.cs
@@ -8,6 +8,7 @@ using System;
 using System.Linq;
 using Grpc.Core;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Proto.Remote
 {
@@ -15,10 +16,11 @@ namespace Proto.Remote
     {
         private static Server _server;
         public static PID EndpointManagerPid { get; private set; }
+        public static PID ActivatorPID { get; private set; }
 
         public static string[] GetKnownKinds()
         {
-            return kinds.Keys.ToArray();
+            return Kinds.Keys.ToArray();
         }
 
         public static void Start(string host, int port)
@@ -32,25 +34,64 @@ namespace Proto.Remote
 
             _server = new Server
             {
-                Services = {Remoting.BindService(new EndpointReader())},
-                Ports = {new ServerPort(host, port, config.ServerCredentials)},
+                Services = { Remoting.BindService(new EndpointReader()) },
+                Ports = { new ServerPort(host, port, config.ServerCredentials) },
             };
             _server.Start();
 
             var boundPort = _server.Ports.Single().BoundPort;
             var addr = host + ":" + boundPort;
             ProcessRegistry.Instance.Address = addr;
-            
-            var props = Actor.FromProducer(() => new EndpointManager(config));
-            EndpointManagerPid = Actor.Spawn(props);
+
+            SpawnEndpointManager(config);
+            SpawnActivator();
 
             Console.WriteLine($"[REMOTING] Starting Proto.Actor server on {addr}");
         }
 
-        private static Dictionary<string, Props> kinds = new Dictionary<string, Props>();
+        private static void SpawnActivator()
+        {
+            var props = Actor.FromProducer(() => new Activator());
+            ActivatorPID = Actor.Spawn(props);
+        }
+
+        private static void SpawnEndpointManager(RemoteConfig config)
+        {
+            var props = Actor.FromProducer(() => new EndpointManager(config));
+            EndpointManagerPid = Actor.Spawn(props);
+        }
+
+        private static Dictionary<string, Props> Kinds { get; } = new Dictionary<string, Props>();
         public static void Register(string kind,Props props)
         {
-            kinds.Add(kind, props);
+            Kinds.Add(kind, props);
+        }
+        public static Props GetKindProps(string kind)
+        {
+            return Kinds[kind];
+        }
+
+        public static PID ActivatorForAddress(string address)
+        {
+            return new PID(address, "activator");
+        }
+
+        public static Task<PID> SpawnAsync(string address, string kind, TimeSpan timeout)
+        {
+            return SpawnNamedAsync(address, "", kind, timeout);
+        }
+
+        private static async Task<PID> SpawnNamedAsync(string address, string name, string kind, TimeSpan timeout)
+        {
+            var activator = ActivatorForAddress(address);
+
+            var res = await activator.RequestAsync<ActorPidResponse>(new ActorPidRequest
+            {
+                Kind = kind,
+                Name = name,
+            }, timeout);
+
+            return res.Pid;
         }
     }
 }
