053e20b6c0f810c8ab44dc012130926a9031c966
Daniel Söderberg <daniel.soderberg85@gmail.com>
2017-03-23 01:52:56 +0100

    added Scheduler api for IContext and added example application

---
+++ examples/Scheduler/Scheduler/Program.cs
@@ -0,0 +1,72 @@
+﻿using Proto;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Scheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var props = Actor.FromProducer(() => new ScheduleActor());
+
+            var pid = Actor.Spawn(props);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Hello { }
+    public class HickUp { }
+    public class AbortHickUp { }
+
+    public class ScheduleActor : IActor
+    {
+        private CancellationTokenSource timer;
+        private int counter = 0;
+
+        public Task ReceiveAsync(IContext context)
+        {
+            switch(context.Message)
+            {
+                case Started _:
+
+                    context.ScheduleTellOnce(TimeSpan.FromSeconds(1), context.Self, new Hello());
+
+                    timer = context.ScheduleTellRepeatedly(TimeSpan.FromSeconds(5), TimeSpan.FromMilliseconds(250), context.Self, new HickUp());
+
+                    break;
+
+                case Hello hl:
+
+                    Console.WriteLine($"Hello Once, let's give you a 5 times hickup!");
+
+                    break;
+
+                case HickUp hu:
+
+                    counter++;
+
+                    Console.WriteLine($"Hello!");
+
+                    if (counter == 5)
+                    {
+                        timer.Cancel();
+
+                        context.Self.Tell(new AbortHickUp());
+                    }
+
+                    break;
+
+                case AbortHickUp ahu:
+
+                    Console.WriteLine($"Aborted hickup after {counter} times");
+
+                    break;
+            }
+
+            return Actor.Done;
+        }
+    }
+}
---
+++ src/Proto.Actor/Scheduler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Proto
+{
+    public static class Scheduler
+    {
+        public static void ScheduleTellOnce(this IContext context, TimeSpan delay, PID target, object message)
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(delay);
+
+                context.Tell(target, message);
+            });
+        }
+
+        public static CancellationTokenSource ScheduleTellRepeatedly(this IContext context, TimeSpan delay, TimeSpan interval, PID target, object message)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            var task = Task.Run(async () =>
+            {
+                await Task.Delay(delay);
+
+                async void Trigger(object _message)
+                {
+                    if (cts.IsCancellationRequested)
+                        return;
+
+                    context.Tell(target, message);
+
+                    await Task.Delay(interval);
+
+                    Trigger(_message);
+                }
+
+                Trigger(message);
+
+            }, cts.Token);
+
+            return cts;
+        }
+    }
+}
