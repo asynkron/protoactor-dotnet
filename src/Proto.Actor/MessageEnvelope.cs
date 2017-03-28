5108582a9673584d833e4501f321d2d22a9e3d1f
rogeralsing <rogeralsing@gmail.com>
2017-03-28 10:07:42 +0200

    .

--- src/Proto.Actor/MessageEnvelope.cs
+++ src/Proto.Actor/MessageEnvelope.cs
@@ -8,20 +8,20 @@ namespace Proto
     {
         public MessageEnvelope(object message, PID sender)
         {
-            Sender = sender;// ?? throw new ArgumentNullException(nameof(sender));
-            Message = message;//?? throw new ArgumentNullException(nameof(message));
+            Sender = sender; // ?? throw new ArgumentNullException(nameof(sender));
+            Message = message; //?? throw new ArgumentNullException(nameof(message));
         }
 
         public MessageEnvelope(object message, PID sender, MessageHeader header)
         {
-            Sender = sender;// ?? throw new ArgumentNullException(nameof(sender));
-            Message = message;// ?? throw new ArgumentNullException(nameof(message));
-            Header = header;// ?? throw new ArgumentNullException(nameof(header));
+            Sender = sender; // ?? throw new ArgumentNullException(nameof(sender));
+            Message = message; // ?? throw new ArgumentNullException(nameof(message));
+            Header = header; // ?? throw new ArgumentNullException(nameof(header));
         }
 
         public PID Sender { get; }
         public object Message { get; }
-        public MessageHeader Header { get; set; }
+        public MessageHeader Header { get; }
     }
 
     public class MessageHeader : Dictionary<string, string>
