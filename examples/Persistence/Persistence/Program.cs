0250eca5edcd47ba6bcdfbed97e8bfbae9497bcf
Daniel Söderberg <daniel.soderberg@solidab.se>
2017-03-30 13:36:57 +0200

    updated couchbase persistence demo

--- examples/Persistence/Persistence/Program.cs
+++ examples/Persistence/Persistence/Program.cs
@@ -23,16 +23,18 @@ class Program
     static void Main(string[] args)
     {
         using (var cluster = GetCluster())
-        using (var bucket = cluster.OpenBucket("protoactor-test"))
+        using (var bucket = cluster.OpenBucket("protoactor_test"))
         {
+            //NOTE: Don't forget to create index for the bucket!
+            //QUERY: CREATE INDEX `persistence` ON `protoactor_test`(type) USING GSI;
+
             var provider = new CouchbaseProvider(bucket);
-            var props = Actor.FromProducer(() => new MyActor())
+
+            var props = Actor.FromProducer(() => new MyPersistenceActor())
                 .WithReceiveMiddleware(Persistence.Using(provider));
+
             var pid = Actor.Spawn(props);
-            pid.Tell(new RenameCommand {Name = "Christian"});
-            pid.Tell(new RenameCommand {Name = "Alex"});
-            pid.Tell(new RenameCommand {Name = "Roger"});
-            Console.WriteLine("Hello World!");
+            
             Console.ReadLine();
         }
     }
@@ -45,7 +47,7 @@ class Program
             {
                 new BucketDefinition
                 {
-                    Name = "protoactor-test",
+                    Name = "protoactor_test",
                     ConnectionPool = new ConnectionPoolDefinition
                     {
                         EnableTcpKeepAlives = true,
@@ -71,29 +73,192 @@ class Program
         return new Cluster(configuration);
     }
 
-    class MyActor : IPersistentActor
+    class MyPersistenceActor : IPersistentActor
     {
+        private PID _loopActor;
         private State _state = new State();
         public Persistence Persistence { get; set; }
+        private class StartLoopActor { }
+        private class TimeToSnapshot { }
+
+        private bool _timerStarted = false;
 
         public async Task ReceiveAsync(IContext context)
         {
             switch (context.Message)
             {
-                case RenameCommand rc:
-                    await Persistence.PersistReceiveAsync(new RenameEvent {Name = rc.Name});
+                case Started msg:
+
+                    Console.WriteLine("MyPersistenceActor - Started");
+
+                    context.Self.Tell(new StartLoopActor());
+
                     break;
-                case RenameEvent re:
-                    _state.Name = re.Name;
-                    Console.WriteLine($"{context.Self.Id} changed name to {_state.Name}");
+                case RecoveryStarted msg:
+
+                    Console.WriteLine("MyPersistenceActor - RecoveryStarted");
+
                     break;
-                case RequestSnapshot rs:
-                    await Persistence.PersistSnapshotAsync(_state);
+                case RecoveryCompleted msg:
+
+                    Console.WriteLine("MyPersistenceActor - RecoveryCompleted");
+
+                    context.Self.Tell(new StartLoopActor());
+
                     break;
-                case State s:
-                    _state = s;
+                case RecoverSnapshot msg:
+                    
+                    if (msg.Data is State ss)
+                    {
+                        _state = ss;
+
+                        Console.WriteLine("MyPersistenceActor - RecoverSnapshot = {0}, Snapshot.Name = {1}", Persistence.SnapshotIndex, ss.Name);
+
+                    }
+
+                    break;
+                case RecoverEvent msg:
+                    
+                    if (msg.Data is RenameEvent recev)
+                    {
+                        Console.WriteLine("MyPersistenceActor - RecoverEvent = {0}, Event.Name = {1}", Persistence.EventIndex, recev.Name);
+                    }
+
+                    break;
+                case PersistedSnapshot msg:
+
+                    await Handle(msg);
+
+                    break;
+                case PersistedEvent msg:
+
+                    Console.WriteLine("MyPersistenceActor - PersistedEvent = {0}", msg.Index);
+
+                    if(msg.Data is RenameEvent rne)
+                    {
+                        _state.Name = rne.Name;
+                    }
+
+                    break;
+                case RequestSnapshot msg:
+
+                    await Handle(context, msg);
+
+                    break;
+                case TimeToSnapshot msg:
+
+                    await Handle(context, msg);
+
+                    break;
+                case StartLoopActor msg:
+
+                    await Handle(context, msg);
+
+                    break;
+                case RenameCommand msg:
+
+                    await Handle(msg);
+
                     break;
             }
+        }
+
+        private Task Handle(PersistedSnapshot message)
+        {
+            Console.WriteLine("MyPersistenceActor - PersistedSnapshot");
+
+            var sn_index = Persistence.SnapshotIndex - 2;
+
+            //await Persistence.State.DeleteSnapshotsAsync(Persistence.Name, sn_index);
+
+            var ev_index = Persistence.EventIndex;
+
+            //await Persistence.State.DeleteEventsAsync(Persistence.Name, ev_index);
+
+            return Actor.Done;
+        }
+
+        private async Task Handle(IContext context, RequestSnapshot message)
+        {
+            Console.WriteLine("MyPersistenceActor - RequestSnapshot");
+
+            await Persistence.PersistSnapshotAsync(_state);
+
+            context.Self.Tell(new TimeToSnapshot());
+        }
+
+        private async Task Handle(IContext context, TimeToSnapshot message)
+        {
+            Console.WriteLine("MyPersistenceActor - TimeToSnapshot");
+
+            await Task.Delay(TimeSpan.FromSeconds(10));
+
+            context.Self.Tell(new RequestSnapshot());
+        }
+
+        private Task Handle(IContext context, StartLoopActor message)
+        {
+            if (_timerStarted) return Actor.Done;
+
+            _timerStarted = true;
+
+            Console.WriteLine("MyPersistenceActor - StartLoopActor");
+
+            var props = Actor.FromProducer(() => new LoopActor(Persistence.EventIndex));
+
+            _loopActor = context.Spawn(props);
+
+            context.Self.Tell(new TimeToSnapshot());
+            
+            return Actor.Done;
+        }
+
+        private async Task Handle(RenameCommand message)
+        {
+            Console.WriteLine("MyPersistenceActor - RenameCommand");
+
+            await Persistence.PersistReceiveAsync(new RenameEvent { Name = message.Name });
+        }
+    }
+
+    class LoopActor : IActor
+    {
+        internal class LoopParentMessage { }
+        long prefixCounter = 0;
+
+        public LoopActor(long startPrefixCounter)
+        {
+            prefixCounter = startPrefixCounter;
+        }
+
+        public Task ReceiveAsync(IContext context)
+        {
+            switch(context.Message)
+            {
+                case Started _:
+
+                    Console.WriteLine("LoopActor - Started");
+
+                    context.Self.Tell(new LoopParentMessage());
+
+                    break;
+                case LoopParentMessage msg:
+
+                    Task.Run(async () => {
+                        
+                        context.Parent.Tell(new RenameCommand { Name = "Daniel-" + prefixCounter });
+
+                        prefixCounter++;
+
+                        await Task.Delay(TimeSpan.FromSeconds(2));
+
+                        context.Self.Tell(new LoopParentMessage());
+                    });
+
+                    break;
+            }
+
+            return Actor.Done;
         }
     }
 }
