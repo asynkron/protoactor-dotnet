7bb9418983e13f6d09ee970fda3f6d2fab9a8ae3
Daniel Söderberg <daniel.soderberg@solidab.se>
2017-04-02 02:35:24 +0200

    # This is a combination of 3 commits. # The first commit's message is: created extenions SimpleScheduler for IContext and added demo app
    # The 2nd commit message will be skipped:
    
    #	Removed dependecy for IContext, instead use these fancy PIDs
    
    # The 3rd commit message will be skipped:
    
    #	Updated example

---
+++ src/Schedulers/Proto.Scheduler.SimpleScheduler/ISimpleScheduler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Proto.Schedulers.SimpleScheduler
+{
+    public interface ISimpleScheduler
+    {
+        ISimpleScheduler ScheduleTellOnce(TimeSpan delay, PID target, object message);
+        ISimpleScheduler ScheduleTellRepeatedly(TimeSpan delay, TimeSpan interval, PID target, object message, out CancellationTokenSource cancellationTokenSource);
+        ISimpleScheduler ScheduleRequestOnce(TimeSpan delay, PID sender, PID target, object message);
+        ISimpleScheduler ScheduleRequestRepeatedly(TimeSpan delay, TimeSpan interval, PID sender, PID target, object message, out CancellationTokenSource cancellationTokenSource);
+    }
+}
---
+++ src/Schedulers/Proto.Scheduler.SimpleScheduler/SimpleScheduler.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Proto.Schedulers.SimpleScheduler
+{
+    public class SimpleScheduler : ISimpleScheduler
+    {
+        public ISimpleScheduler ScheduleTellOnce(TimeSpan delay, PID target, object message)
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(delay);
+
+                target.Tell(message);
+            });
+
+            return this;
+        }
+
+        public ISimpleScheduler ScheduleTellRepeatedly(TimeSpan delay, TimeSpan interval, PID target, object message, out CancellationTokenSource cancellationTokenSource)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            var task = Task.Run(async () =>
+            {
+                await Task.Delay(delay);
+
+                async void Trigger(object _message)
+                {
+                    if (cts.IsCancellationRequested)
+                        return;
+
+                    target.Tell(message);
+
+                    await Task.Delay(interval);
+
+                    Trigger(_message);
+                }
+
+                Trigger(message);
+
+            }, cts.Token);
+
+            cancellationTokenSource = cts;
+
+            return this;
+        }
+
+        public ISimpleScheduler ScheduleRequestOnce(TimeSpan delay, PID sender, PID target, object message)
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(delay);
+
+                target.Request(message, sender);
+            });
+
+            return this;
+        }
+
+        public ISimpleScheduler ScheduleRequestRepeatedly(TimeSpan delay, TimeSpan interval, PID sender, PID target, object message, out CancellationTokenSource cancellationTokenSource)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            var task = Task.Run(async () =>
+            {
+                await Task.Delay(delay);
+
+                async void Trigger(object _message)
+                {
+                    if (cts.IsCancellationRequested)
+                        return;
+
+                    target.Request(message, sender);
+
+                    await Task.Delay(interval);
+
+                    Trigger(_message);
+                }
+
+                Trigger(message);
+
+            }, cts.Token);
+
+            cancellationTokenSource = cts;
+
+            return this;
+        }
+    }
+}
