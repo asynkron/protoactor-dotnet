e7dd406f064d12a038803657b00fa8db32f4bb63
Tom Liversidge <tom@liversidge.me>
2017-04-07 21:38:51 +0300

    Remoting tests
    This works by launching a console app that represents a remote node via System.Diagnostics.Process using "dotnet <path to dll>".
    This is used in an xunit fixture so shared amongst tests.
    
    Tested xplatform on win10 and mac
    Requires Proto.Remote.Tests.Node to be built first (solution should do this)

---
+++ tests/Proto.Remote.Tests.Node/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Proto.Remote.Tests.Messages;
+
+namespace Proto.Remote.Tests.Node
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serialization.RegisterFileDescriptor(Tests.Messages.ProtosReflection.Descriptor);
+            Remote.Start("127.0.0.1", 12000);
+            Actor.SpawnNamed(Actor.FromProducer(() => new EchoActor()), "remote");
+            Console.ReadLine();
+        }
+    }
+
+    public class EchoActor : IActor
+    {
+        private PID _sender;
+
+        public Task ReceiveAsync(IContext context)
+        {
+            switch (context.Message)
+            {
+                case StartRemote sr:
+                    Console.WriteLine("Starting");
+                    _sender = sr.Sender;
+                    context.Respond(new Start());
+                    return Actor.Done;
+                case Ping _:
+                    _sender.Tell(new Pong());
+                    return Actor.Done;
+                default:
+                    return Actor.Done;
+            }
+        }
+    }
+}
---
+++ tests/Proto.Remote.Tests/RemoteActorSystem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+
+namespace Proto.Remote.Tests
+{
+    public class RemoteActorSystem : IDisposable
+    {
+        private static string NodeAppPath => @"Proto.Remote.Tests.Node/bin/Debug/netcoreapp1.1/Proto.Remote.Tests.Node.dll";
+
+        private readonly System.Diagnostics.Process _process;
+
+        public RemoteActorSystem()
+        {
+            var testsDirectory = System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent;
+           
+            var nodeDllPath = $@"{testsDirectory.FullName}/{NodeAppPath}";
+            Console.WriteLine($"NodeDLL path: {nodeDllPath}");
+            _process = new System.Diagnostics.Process
+            {
+                StartInfo =
+                {
+                    Arguments = nodeDllPath,
+                    CreateNoWindow = false,
+                    UseShellExecute = false,
+                    FileName = "dotnet"
+                }
+            };
+            _process.Start();
+            Remote.Start("127.0.0.1", 12001);
+
+            Serialization.RegisterFileDescriptor(Messages.ProtosReflection.Descriptor);
+
+            Thread.Sleep(2000);
+        }
+
+        public void Dispose()
+        {
+            if (_process == null || _process.HasExited)
+                return;
+
+            _process.Kill();
+        }
+    }
+
+    [CollectionDefinition("RemoteTests")]
+    public class RemoteCollection : ICollectionFixture<RemoteActorSystem>
+    {
+    }
+}
