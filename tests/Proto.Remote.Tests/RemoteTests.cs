78b407ba6ae82a3e3ebb2e56c983d342abb2472d
Tom Liversidge <tom@liversidge.me>
2017-04-10 19:19:00 +0300

    simplifying remote tests

--- tests/Proto.Remote.Tests/RemoteTests.cs
+++ tests/Proto.Remote.Tests/RemoteTests.cs
@@ -12,64 +12,28 @@ namespace Proto.Remote.Tests
         [Fact]
         public async void CanSendAndReceiveRemote()
         {
-            var props = Actor.FromProducer(() => new LocalActor());
-            var pid = Actor.Spawn(props);
-            
             var remoteActor = new PID("127.0.0.1:12000", "remote");
-            await remoteActor.RequestAsync<Start>(new StartRemote { Sender = pid }, TimeSpan.FromMilliseconds(2500));
-
-            remoteActor.Tell(new Ping());
-            await Task.Delay(TimeSpan.FromSeconds(2));
-            var responseReceived = await pid.RequestAsync<bool>("?", TimeSpan.FromMilliseconds(2500));
-
-            Assert.True(responseReceived);
+            var pong = await remoteActor.RequestAsync<Pong>(new Ping { Message = "Hello" }, TimeSpan.FromMilliseconds(5000));
+            Assert.Equal("127.0.0.1:12000 Hello", pong.Message);
         }
 
         [Fact]
         public async void WhenRemoteActorNotFound_RequestAsyncTimesout()
         {
-            var props = Actor.FromProducer(() => new LocalActor());
-            var pid = Actor.Spawn(props);
-
             var unknownRemoteActor = new PID("127.0.0.1:12000", "doesn't exist");
             await Assert.ThrowsAsync<TimeoutException>(async () =>
             {
-                await unknownRemoteActor.RequestAsync<Start>(new StartRemote { Sender = pid }, TimeSpan.FromMilliseconds(2000));
+                await unknownRemoteActor.RequestAsync<Pong>(new Ping { Message = "Hello" }, TimeSpan.FromMilliseconds(2000));
             });
         }
 
         [Fact]
         public async void CanSpawnRemoteActor()
         {
-            var props = Actor.FromProducer(() => new LocalActor());
-            var pid = Actor.Spawn(props);
             var remoteActorName = Guid.NewGuid().ToString();
             var remoteActor = await Remote.SpawnNamedAsync("127.0.0.1:12000", remoteActorName, "remote", TimeSpan.FromSeconds(5));
-            await remoteActor.RequestAsync<Start>(new StartRemote { Sender = pid }, TimeSpan.FromMilliseconds(2500));
-            remoteActor.Tell(new Ping());
-            await Task.Delay(TimeSpan.FromSeconds(2));
-
-            var responseReceived = await pid.RequestAsync<bool>("?", TimeSpan.FromMilliseconds(2500));
-            Assert.True(responseReceived);
-        }
-
-        public class LocalActor : IActor
-        {
-            private bool _pongReceived;
-
-            public Task ReceiveAsync(IContext context)
-            {
-                switch (context.Message)
-                {
-                    case Pong _:
-                        _pongReceived = true;
-                        break;
-                    case string msg when msg == "?":
-                        context.Sender.Tell(_pongReceived);
-                        break;
-                }
-                return Actor.Done;
-            }
+            var pong = await remoteActor.RequestAsync<Pong>(new Ping{Message="Hello"}, TimeSpan.FromMilliseconds(5000));
+            Assert.Equal("127.0.0.1:12000 Hello", pong.Message);
         }
     }
 }
