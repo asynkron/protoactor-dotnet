8bc86cc1403af710da60dffa6fa98875a8c663e1
rogeralsing <rogeralsing@gmail.com>
2017-06-17 13:59:48 +0200

    add new grain generator

---
+++ protobuf/ProtoGrainGenerator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Google.Protobuf.Reflection;
+using GrainGenerator;
+
+namespace ProtoGrainGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var set = new FileDescriptorSet();
+            var r = File.OpenText(@"c:\git\protoactor-dotnet\examples\ClusterGrainHelloWorld\Messages\Protos.proto");
+            set.Add("my.proto", true, r);
+
+            set.Process();
+            var gen = new GrainGen();
+            var res = gen.Generate(set).ToList();
+        }
+    }
+}
