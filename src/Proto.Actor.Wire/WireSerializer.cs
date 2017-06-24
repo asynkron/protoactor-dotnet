d79e7af17f534df6004d34feae5f502bd848d6b2
rogeralsing <rogeralsing@gmail.com>
2017-06-24 12:55:55 +0200

    Wire serializer

---
+++ src/Proto.Actor.Wire/WireSerializer.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Google.Protobuf;
+using Proto.Remote;
+using Wire;
+
+namespace Proto.Serialization
+{
+    public class WireSerializer : ISerializer
+    {
+        private readonly Serializer _serializer = new Serializer();
+        
+        public ByteString Serialize(object obj)
+        {
+            var ms = new MemoryStream();
+            _serializer.Serialize(obj,ms);
+            var arr = ms.ToArray();
+            ms.Dispose();
+            return ByteString.CopyFrom(arr);
+        }
+
+        public object Deserialize(ByteString bytes, string typeName)
+        {
+            var arr = bytes.ToByteArray();
+            var ms = new MemoryStream(arr) {Position = 0};
+            var obj = _serializer.Deserialize(ms);
+            ms.Dispose();
+            return obj;
+        }
+
+        public string GetTypeName(object message)
+        {
+            return "";
+        }
+    }
+}
