83bbccfb3ef7de08ae63eb9bf989ea6a02c518b3
rogeralsing <rogeralsing@gmail.com>
2017-09-28 22:15:37 +0200

    added cluster test slot

---
+++ tests/Proto.Cluster.Tests/TestClusterProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+namespace Proto.Cluster.Tests
+{
+    public class TestClusterProvider : IClusterProvider
+    {
+        public Task RegisterMemberAsync(string clusterName, string h, int p, string[] kinds)
+        {
+            return Task.FromResult(0);
+        }
+
+        public void MonitorMemberStatusChanges()
+        {
+        }
+
+        public Task DeregisterMemberAsync()
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task Shutdown()
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
