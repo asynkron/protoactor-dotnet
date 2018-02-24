a799c457d8d5c50aa82c35acce28165d13fd635c
Andrea Balducci <mtb.snowboard@gmail.com>
2018-02-24 15:52:14 +0100

    added ActorPropsRegistry.RegisterProps(Type, Func<Props, Props> (#378)

--- tests/Proto.Actor.Extensions.Tests/ActorFactoryTest.cs
+++ tests/Proto.Actor.Extensions.Tests/ActorFactoryTest.cs
@@ -1,26 +1,10 @@
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Threading.Tasks;
 using Proto;
 using Xunit;
 
 namespace Proto.ActorExtensions.Tests
 {
-    public class SampleActor : IActor
-    {
-        public static bool Created = false;
-
-        public SampleActor()
-        {
-            Created = true;
-        }
-
-        public Task ReceiveAsync(IContext context)
-        {
-            return Actor.Done;
-        }
-    }
-
     public class ActorFactoryTest
     {
         [Fact]
@@ -38,6 +22,42 @@ namespace Proto.ActorExtensions.Tests
             pid.Stop();
 
             Assert.True(SampleActor.Created);
+        }
+
+        [Fact]
+        public void should_register_by_type()
+        {
+            var services = new ServiceCollection();
+            var created = false;
+
+            Func<IActor> producer = () =>
+            {
+                created = true;
+                return new SampleActor();
+            };
+
+            services.AddProtoActor(register => register.RegisterProps(typeof(SampleActor), p => p.WithProducer(producer)));
+
+            var provider = services.BuildServiceProvider();
+            var factory = provider.GetRequiredService<IActorFactory>();
+
+            var pid = factory.GetActor<SampleActor>();
+
+            pid.Stop();
+
+            Assert.True(created);
+        }
+
+        [Fact]
+        public void should_throw_if_not_actor_type()
+        {
+            var services = new ServiceCollection();
+            var ex = Assert.Throws<InvalidOperationException>(() =>
+            {
+                services.AddProtoActor(register => register.RegisterProps(GetType(), p => p));
+            });
+
+            Assert.Equal($"Type {GetType().FullName} must implement {typeof(IActor).FullName}", ex.Message);
         }
     }
 }
