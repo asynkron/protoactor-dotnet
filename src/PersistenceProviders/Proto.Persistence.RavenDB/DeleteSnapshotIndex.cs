a1c96f8de307b4681f7a62d4e8390e58e248f12c
Potter Dai <potterdaica@gmail.com>
2019-05-02 02:53:32 -0400

    initial change.

--- src/PersistenceProviders/Proto.Persistence.RavenDB/DeleteSnapshotIndex.cs
+++ src/PersistenceProviders/Proto.Persistence.RavenDB/DeleteSnapshotIndex.cs
@@ -1,10 +1,5 @@
 ﻿using System.Linq;
-#if NET452
-using Raven.Abstractions.Indexing;
-using Raven.Client.Indexes;
-#else
 using Raven.Client.Documents.Indexes;
-#endif
 
 namespace Proto.Persistence.RavenDB
 {
@@ -20,13 +15,7 @@ namespace Proto.Persistence.RavenDB
                           };
 
             Index(x => x.ActorName, FieldIndexing.Default);
-#if NET452
-            Sort(x => x.ActorName, SortOptions.String);
-#endif
             Index(x => x.Index, FieldIndexing.Default);
-#if NET452
-            Sort(x => x.Index, SortOptions.Long);
-#endif
         }
     }
 }
