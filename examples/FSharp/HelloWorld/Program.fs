0f061d3909d43b37a4dc1b15bc83a6ed78a34a4e
Andreas Vilinski <vilinski@yahoo.de>
2019-06-24 00:11:35 +0200

    F# HelloWorld

---
+++ examples/FSharp/HelloWorld/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open Proto
+
+type HelloActor() =
+    interface IActor with
+        member __.ReceiveAsync ctx =
+            match ctx.Message with
+            | :? {| Who : string |} as hello ->
+                printfn "Hello %s" hello.Who
+                exit 0
+            | _ -> ()
+            Actor.Done
+
+[<EntryPoint>]
+let main argv =
+    let ctx = RootContext()
+    let props = Props.FromProducer(fun () -> HelloActor() :> IActor)
+    let pid = ctx.Spawn props
+    ctx.Send(pid, {| Who = "ProtoActor" |})
+    Console.ReadLine() |> ignore
+    0
