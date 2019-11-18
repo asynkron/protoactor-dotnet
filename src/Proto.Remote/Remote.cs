c9ba1c1cb174315ab7b6b8cf4ae50aece069bdf2
Jerome Esnault <ejerome@users.noreply.github.com>
2019-11-18 12:00:16 +0100

    Update copyright license of modified code

--- src/Proto.Remote/Remote.cs
+++ src/Proto.Remote/Remote.cs
@@ -4,6 +4,16 @@
 //   </copyright>
 // -----------------------------------------------------------------------
 // Modified file in context of repo fork : https://github.com/Optis-World/protoactor-dotnet
+// Copyright 2019 ANSYS, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 using System;
 using System.Collections.Generic;
