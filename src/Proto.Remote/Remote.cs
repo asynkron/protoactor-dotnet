33a0e2784514b110793f626d11a7a76db895f0c2
Jerôme ESNAULT <ejerome@users.noreply.github.com>
2020-01-23 19:30:25 +0100

    Update Remote.cs to add UnregisterKnownKind (#484)
    * Update Remote.cs to add UnregisterKnownKind
    
    * Update copyright license of modified code

--- src/Proto.Remote/Remote.cs
+++ src/Proto.Remote/Remote.cs
@@ -3,6 +3,17 @@
 //       Copyright (C) 2015-2018 Asynkron HB All rights reserved
 //   </copyright>
 // -----------------------------------------------------------------------
+// Modified file in context of repo fork : https://github.com/Optis-World/protoactor-dotnet
+// Copyright 2019 ANSYS, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 using System;
 using System.Collections.Generic;
@@ -34,6 +45,12 @@ namespace Proto.Remote
             Kinds.Add(kind, props);
         }
 
+        // Modified class in context of repo fork : https://github.com/Optis-World/protoactor-dotnet
+        public static void UnregisterKnownKind(string kind)
+        {
+            Kinds.Remove(kind);
+        }
+        
         public static Props GetKnownKind(string kind)
         {
             if (Kinds.TryGetValue(kind, out var props)){
