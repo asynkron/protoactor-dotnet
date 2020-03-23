asynkron/protoactor-dotnet

commit ad2b58ae9ac754bb8a4c7dfa72409d2fd0fdf53d

Fixing nullable warnings (#506)
Removing obsolete references

src/Proto.Actor/Props.cs
@@ -14,9 +14,9 @@ namespace Proto
     public sealed class Props
     {
         private Spawner? _spawner;
-        public Func<IActor> Producer { get; private set; }
-        public Func<IMailbox> MailboxProducer { get; private set; } = ProduceDefaultMailbox;
-        public ISupervisorStrategy GuardianStrategy { get; private set; }
+        public Producer Producer { get; private set; } = () => null;
+        public MailboxProducer MailboxProducer { get; private set; } = ProduceDefaultMailbox;
+        public ISupervisorStrategy GuardianStrategy { get; private set; } = Supervision.DefaultStrategy;
         public ISupervisorStrategy SupervisorStrategy { get; private set; } = Supervision.DefaultStrategy;
         public IDispatcher Dispatcher { get; private set; } = Dispatchers.DefaultDispatcher;
 
@@ -24,8 +24,8 @@ namespace Proto
             new List<Func<Receiver, Receiver>>();
 
         public IList<Func<Sender, Sender>> SenderMiddleware { get; private set; } = new List<Func<Sender, Sender>>();
-        public Receiver ReceiverMiddlewareChain { get; private set; }
-        public Sender SenderMiddlewareChain { get; private set; }
+        public Receiver? ReceiverMiddlewareChain { get; private set; }
+        public Sender? SenderMiddlewareChain { get; private set; }
 
         public IList<Func<IContext, IContext>> ContextDecorator { get; private set; } =
             new List<Func<IContext, IContext>>();
@@ -42,12 +42,13 @@ namespace Proto
 
         private static IMailbox ProduceDefaultMailbox() => UnboundedMailbox.Create();
 
-        public PID DefaultSpawner(ActorSystem system, string name, Props props, PID parent)
+        public static PID DefaultSpawner(ActorSystem system, string name, Props props, PID? parent)
         {
             var mailbox = props.MailboxProducer();
             var dispatcher = props.Dispatcher;
             var process = new ActorProcess(system, mailbox);
             var (pid, absent) = system.ProcessRegistry.TryAdd(name, process);
+
             if (!absent)
             {
                 throw new ProcessNameExistException(name, pid);
@@ -61,50 +62,58 @@ namespace Proto
             return pid;
         }
 
-        public Props WithProducer(Func<IActor> producer) => Copy(props => props.Producer = producer);
+        public Props WithProducer(Producer producer) => Copy(props => props.Producer = producer);
 
         public Props WithDispatcher(IDispatcher dispatcher) => Copy(props => props.Dispatcher = dispatcher);
 
-        public Props WithMailbox(Func<IMailbox> mailboxProducer) =>
-            Copy(props => props.MailboxProducer = mailboxProducer);
+        public Props WithMailbox(MailboxProducer mailboxProducer) => Copy(props => props.MailboxProducer = mailboxProducer);
 
-        public Props WithContextDecorator(params Func<IContext, IContext>[] contextDecorator) => Copy(props =>
-            {
-                props.ContextDecorator = ContextDecorator.Concat(contextDecorator).ToList();
-                props.ContextDecoratorChain = props.ContextDecorator.Reverse()
-                    .Aggregate((Func<IContext, IContext>)DefaultContextDecorator,
-                        (inner, outer) => ctx => outer(inner(ctx))
-                    );
-            }
-        );
+        public Props WithContextDecorator(params Func<IContext, IContext>[] contextDecorator)
+            => Copy(
+                props =>
+                {
+                    props.ContextDecorator = ContextDecorator.Concat(contextDecorator).ToList();
 
-        public Props WithGuardianSupervisorStrategy(ISupervisorStrategy guardianStrategy) =>
-            Copy(props => props.GuardianStrategy = guardianStrategy);
+                    props.ContextDecoratorChain = props.ContextDecorator.Reverse()
+                        .Aggregate(
+                            (Func<IContext, IContext>) DefaultContextDecorator,
+                            (inner, outer) => ctx => outer(inner(ctx))
+                        );
+                }
+            );
 
-        public Props WithChildSupervisorStrategy(ISupervisorStrategy supervisorStrategy) =>
-            Copy(props => props.SupervisorStrategy = supervisorStrategy);
+        public Props WithGuardianSupervisorStrategy(ISupervisorStrategy guardianStrategy) => Copy(props => props.GuardianStrategy = guardianStrategy);
 
-        public Props WithReceiverMiddleware(params Func<Receiver, Receiver>[] middleware) => Copy(props =>
-            {
-                props.ReceiverMiddleware = ReceiverMiddleware.Concat(middleware).ToList();
-                props.ReceiverMiddlewareChain = props.ReceiverMiddleware.Reverse()
-                    .Aggregate((Receiver)Middleware.Receive, (inner, outer) => outer(inner));
-            }
-        );
+        public Props WithChildSupervisorStrategy(ISupervisorStrategy supervisorStrategy)
+            => Copy(props => props.SupervisorStrategy = supervisorStrategy);
 
-        public Props WithSenderMiddleware(params Func<Sender, Sender>[] middleware) => Copy(props =>
-            {
-                props.SenderMiddleware = SenderMiddleware.Concat(middleware).ToList();
-                props.SenderMiddlewareChain = props.SenderMiddleware.Reverse()
-                    .Aggregate((Sender)Middleware.Sender, (inner, outer) => outer(inner));
-            }
-        );
+        public Props WithReceiverMiddleware(params Func<Receiver, Receiver>[] middleware)
+            => Copy(
+                props =>
+                {
+                    props.ReceiverMiddleware = ReceiverMiddleware.Concat(middleware).ToList();
+
+                    props.ReceiverMiddlewareChain = props.ReceiverMiddleware.Reverse()
+                        .Aggregate((Receiver) Middleware.Receive, (inner, outer) => outer(inner));
+                }
+            );
+
+        public Props WithSenderMiddleware(params Func<Sender, Sender>[] middleware)
+            => Copy(
+                props =>
+                {
+                    props.SenderMiddleware = SenderMiddleware.Concat(middleware).ToList();
+
+                    props.SenderMiddlewareChain = props.SenderMiddleware.Reverse()
+                        .Aggregate((Sender) Middleware.Sender, (inner, outer) => outer(inner));
+                }
+            );
 
         public Props WithSpawner(Spawner spawner) => Copy(props => props.Spawner = spawner);
 
         private Props Copy(Action<Props> mutator)
         {
-            var props = new Props()
+            var props = new Props
             {
                 Dispatcher = Dispatcher,
                 MailboxProducer = MailboxProducer,
@@ -123,10 +132,14 @@ namespace Proto
             return props;
         }
 
-        internal PID Spawn(ActorSystem system, string name, PID parent) => Spawner(system, name, this, parent);
-        public static Props FromProducer(Func<IActor> producer) => new Props().WithProducer(producer);
+        internal PID Spawn(ActorSystem system, string name, PID? parent) => Spawner(system, name, this, parent);
+        public static Props FromProducer(Producer producer) => new Props().WithProducer(producer);
         public static Props FromFunc(Receive receive) => FromProducer(() => new EmptyActor(receive));
     }
 
-    public delegate PID Spawner(ActorSystem system, string id, Props props, PID parent);
+    public delegate PID Spawner(ActorSystem system, string id, Props props, PID? parent);
+
+    public delegate IActor? Producer();
+
+    public delegate IMailbox MailboxProducer();
 }

commit 653f135a3a4045b7230ac6809952a7fa5608c8e5

Fixing tests that were broken by #506

src/Proto.Actor/Props.cs
@@ -16,7 +16,7 @@ namespace Proto
         private Spawner? _spawner;
         public Producer Producer { get; private set; } = () => null;
         public MailboxProducer MailboxProducer { get; private set; } = ProduceDefaultMailbox;
-        public ISupervisorStrategy GuardianStrategy { get; private set; } = Supervision.DefaultStrategy;
+        public ISupervisorStrategy? GuardianStrategy { get; private set; }
         public ISupervisorStrategy SupervisorStrategy { get; private set; } = Supervision.DefaultStrategy;
         public IDispatcher Dispatcher { get; private set; } = Dispatchers.DefaultDispatcher;
 