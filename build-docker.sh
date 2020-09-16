10f0a5204862661abfa101d89c4d110a5dc225b8
rogerjohansson <rogeralsing@gmail.com>
2020-09-16 12:55:14 +0200

    .

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,7 @@
 
 docker build -f dockerfile-reader . -t rogeralsing/clusterreader
 docker build -f dockerfile-sender . -t rogeralsing/clustersender
+
+kubectl run mongo --image=mongo:4 --expose --port 27017
+kubectl port-forward service/mongo 27017:27017
+kubectl apply --filename service.yaml
