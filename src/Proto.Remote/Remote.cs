423c4ed2ba03bb97ec42bfd51985dfcdc52bab30
rogerjohansson <rogeralsing@gmail.com>
2020-10-04 17:44:45 +0200

    remove blank line additions with weird copyrights

--- src/Proto.Remote/Remote.cs
+++ src/Proto.Remote/Remote.cs
@@ -3,20 +3,8 @@
 //       Copyright (C) 2015-2020 Asynkron AB All rights reserved
 //   </copyright>
 // -----------------------------------------------------------------------
-// Modified file in context of repo fork : https://github.com/Optis-World/protoactor-dotnet
-// Copyright 2019 ANSYS, Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
@@ -50,10 +38,8 @@ namespace Proto.Remote
         public string[] GetKnownKinds() => Config.KnownKinds.Keys.ToArray();
 
         public void RegisterKnownKind(string kind, Props props) => Config.KnownKinds.Add(kind, props);
-
-        // Modified class in context of repo fork : https://github.com/Optis-World/protoactor-dotnet
+        
         public void UnregisterKnownKind(string kind) => Config.KnownKinds.Remove(kind);
-
         public Props GetKnownKind(string kind)
         {
             if (!Config.KnownKinds.TryGetValue(kind, out var props))
