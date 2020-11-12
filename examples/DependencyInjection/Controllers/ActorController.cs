74f07449299b866d8988e9315c00007885d774c3
rogerjohansson <rogeralsing@gmail.com>
2020-11-12 15:51:50 +0100

    Di example Asp.NET Core

---
+++ examples/DependencyInjection/Controllers/ActorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proto;
+
+namespace DependencyInjection.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ActorController : ControllerBase
+    {
+        private readonly ActorSystem _actorSystem;
+
+        public ActorController(ActorSystem actorSystem)
+        {
+            _actorSystem = actorSystem;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            //Use your actors here
+            // _actorSystem.***
+            
+            return null;
+        }
+    }
+}
