f6d82056f88a441a823103edd5cfb11b0acaa14b
rogerjohansson <rogeralsing@gmail.com>
2020-11-21 19:48:34 +0100

    Utility functions for IEnumerable

--- src/Proto.Actor/Interactive/InteractiveExtensions.cs
+++ src/Proto.Actor/Interactive/InteractiveExtensions.cs
@@ -14,6 +14,52 @@ namespace Proto.Interactive
         public static async Task ParallelForEach<T>(this IEnumerable<T> self, Func<T, Task> body,
             int concurrencyLevel = 10)
         {
+            var (s,routees,router) = Start(body, concurrencyLevel);
+
+            foreach (var msg in self)
+            {
+                s.Root.Send(router, msg);
+            }
+
+            await Shutdown(routees, s, router);
+        }
+        
+        public static async Task ParallelForEach<T>(this IAsyncEnumerable<T> self, Func<T, Task> body,
+            int concurrencyLevel = 10)
+        {
+            var (s,routees,router) = Start(body, concurrencyLevel);
+
+            await foreach (var msg in self)
+            {
+                s.Root.Send(router, msg);
+            }
+
+            await Shutdown(routees, s, router);
+        }
+
+        public static Task ParallelForEach<T>(this IEnumerable<T> self, Action<T> body,
+            int concurrencyLevel = 10) =>
+            self.ParallelForEach(x =>
+                {
+                    body(x);
+                    return Task.CompletedTask;
+                }, concurrencyLevel
+            );
+
+
+
+        private static async Task Shutdown(PID[] routees, ActorSystem s, PID router)
+        {
+            foreach (var routee in routees)
+            {
+                await s.Root.PoisonAsync(routee);
+            }
+
+            await s.Root.StopAsync(router);
+        }
+
+        private static (ActorSystem, PID[], PID) Start<T>(Func<T, Task> body, int concurrencyLevel)
+        {
             var s = new ActorSystem();
             var props = Props.FromFunc(async ctx =>
                 {
@@ -31,27 +77,7 @@ namespace Proto.Interactive
 
             var routerProps = s.Root.NewRoundRobinGroup(routees);
             var router = s.Root.Spawn(routerProps);
-
-            foreach (var msg in self)
-            {
-                s.Root.Send(router, msg);
-            }
-
-            foreach (var routee in routees)
-            {
-                await s.Root.PoisonAsync(routee);
-            }
-
-            await s.Root.StopAsync(router);
+            return (s,routees,router);
         }
-        
-        public static Task ParallelForEach<T>(this IEnumerable<T> self, Action<T> body,
-            int concurrencyLevel = 10) =>
-            self.ParallelForEach(x =>
-                {
-                    body(x);
-                    return Task.CompletedTask;
-                }, concurrencyLevel
-            );
     }
 }
