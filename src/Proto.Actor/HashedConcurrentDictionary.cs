f32c9edd9ac0bc2bf4b4efed72dd0f55fd0db427
Magne Helleborg <magne.helleborg@gmail.com>
2020-12-02 18:33:48 +0100

    Fixes bug mentioned in #595. Fix courtesy of Milosz Krajewski (#597)

--- src/Proto.Actor/HashedConcurrentDictionary.cs
+++ src/Proto.Actor/HashedConcurrentDictionary.cs
@@ -29,7 +29,7 @@ namespace Proto
 
         private Partition GetPartition(string key)
         {
-            var hash = Math.Abs(key.GetHashCode()) % HashSize;
+            var hash = key.GetHashCode() & 0x7FFFFFFF % HashSize;
             var p = _partitions[hash];
             return p;
         }
