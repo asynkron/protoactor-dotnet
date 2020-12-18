99b63f5e6a41f5646876b599c2fb29ec0ed0309b
Piechowicz Damian <dampiecho@gmail.com>
2020-12-18 20:53:58 +0100

    Proto host (#623)

--- benchmarks/HostedService/ProtoHost.cs
+++ benchmarks/HostedService/ProtoHost.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Proto;
 using Proto.Cluster;
 
 namespace HostedService
@@ -10,31 +12,53 @@ namespace HostedService
     {
         private Cluster _cluster;
         private ILogger<ProtoHost> _logger;
+        private readonly IHostApplicationLifetime _appLifetime;
 
-
-        public ProtoHost(Cluster cluster, ILogger<ProtoHost> logger)
+        public ProtoHost(Cluster cluster, ILogger<ProtoHost> logger, IHostApplicationLifetime appLifetime)
         {
             _cluster = cluster;
             _logger = logger;
+            _appLifetime = appLifetime;
         }
-        
+
         public Task StartAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("Starting cluster...");
+            _appLifetime.ApplicationStarted.Register(() => Task.Run(() => RunRequestLoop(), _appLifetime.ApplicationStopping));
+            _appLifetime.ApplicationStopping.Register(OnStopping);
             return Task.CompletedTask;
-            
+
         }
 
-        public async Task StopAsync(CancellationToken cancellationToken)
+        //flood the system, to see how it reacts upon shutdown.
+        private async Task RunRequestLoop()
+        {
+            await Task.Yield();
+
+            var rnd = new Random();
+
+            while (!_appLifetime.ApplicationStopping.IsCancellationRequested)
+            {
+                var id = rnd.Next(0, 1000);
+                _ = _cluster.RequestAsync<int>($"abc{id}", "kind", 123, _appLifetime.ApplicationStopping);
+            }
+        }
+
+        private void OnStopping()
         {
             _logger.LogWarning("Shutting down cluster...");
             var shutdown = _cluster.ShutdownAsync(true);
             var timeout = Task.Delay(15000);
-            await Task.WhenAny(shutdown, timeout);
+            Task.WhenAny(shutdown, timeout).GetAwaiter().GetResult();
             if (shutdown.IsCompleted)
                 _logger.LogWarning("Shut down cluster...");
             else
                 _logger.LogError("Shut down cluster timed out...");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
         }
     }
 }
