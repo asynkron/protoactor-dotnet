8bd6eb5bcaeb6b278843ec0d0d4437eff8561adc
rogerjohansson <rogeralsing@gmail.com>
2020-12-29 14:54:34 +0100

    decorator context

--- src/Proto.Cluster/Cache/CacheInvalidationExtensions.cs
+++ src/Proto.Cluster/Cache/CacheInvalidationExtensions.cs
@@ -4,10 +4,9 @@
 // </copyright>
 // -----------------------------------------------------------------------
 using System;
-using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Proto.Context;
 
 namespace Proto.Cluster.Cache
 {
@@ -32,27 +31,19 @@ namespace Proto.Cluster.Cache
                 }
             );
 
-        private class CacheInvalidationContext : IContext
+        private class CacheInvalidationContext : DecoratorContext
         {
-            private readonly IContext _context;
             private readonly ClusterCacheInvalidation _plugin;
             private Action<MessageEnvelope>? _callBack;
 
-            public CacheInvalidationContext(IContext context, ClusterCacheInvalidation cacheInvalidation)
+            public CacheInvalidationContext(IContext context, ClusterCacheInvalidation cacheInvalidation) : base(context)
             {
-                _context = context;
                 _plugin = cacheInvalidation;
             }
 
-            public PID? Parent => _context.Parent;
-            public PID? Self => _context.Self;
-            public PID? Sender => _context.Sender;
-            public IActor? Actor => _context.Actor;
-            public ActorSystem System => _context.System;
-
-            public async Task Receive(MessageEnvelope envelope)
+            public override async Task Receive(MessageEnvelope envelope)
             {
-                await _context.Receive(envelope);
+                await InnerContext.Receive(envelope);
 
                 if (envelope.Message is ClusterInit init)
                 {
@@ -63,58 +54,6 @@ namespace Proto.Cluster.Cache
                     _callBack?.Invoke(envelope);
                 }
             }
-
-            public MessageHeader Headers => _context.Headers;
-            public object? Message => _context.Message;
-
-            public void Send(PID target, object message) => _context.Send(target, message);
-
-            public void Request(PID target, object message) => _context.Request(target, message);
-
-            public void Request(PID target, object message, PID? sender) => _context.Request(target, message, sender);
-
-            public Task<T> RequestAsync<T>(PID target, object message, TimeSpan timeout) => _context.RequestAsync<T>(target, message, timeout);
-
-            public Task<T> RequestAsync<T>(PID target, object message, CancellationToken cancellationToken)
-                => _context.RequestAsync<T>(target, message, cancellationToken);
-
-            public Task<T> RequestAsync<T>(PID target, object message) => _context.RequestAsync<T>(target, message);
-
-            public PID Spawn(Props props) => _context.Spawn(props);
-
-            public PID SpawnNamed(Props props, string name) => _context.SpawnNamed(props, name);
-
-            public PID SpawnPrefix(Props props, string prefix) => _context.SpawnPrefix(props, prefix);
-
-            public void Stop(PID pid) => _context.Stop(pid);
-
-            public Task StopAsync(PID pid) => _context.StopAsync(pid);
-
-            public void Poison(PID pid) => _context.Poison(pid);
-
-            public Task PoisonAsync(PID pid) => _context.PoisonAsync(pid);
-
-            public CancellationToken CancellationToken => _context.CancellationToken;
-            public TimeSpan ReceiveTimeout => _context.ReceiveTimeout;
-            public IReadOnlyCollection<PID> Children => _context.Children;
-
-            public void Respond(object message) => _context.Respond(message);
-
-            public void Stash() => _context.Stash();
-
-            public void Watch(PID pid) => _context.Watch(pid);
-
-            public void Unwatch(PID pid) => _context.Unwatch(pid);
-
-            public void SetReceiveTimeout(TimeSpan duration) => _context.SetReceiveTimeout(duration);
-
-            public void CancelReceiveTimeout() => _context.CancelReceiveTimeout();
-
-            public void Forward(PID target) => _context.Forward(target);
-
-            public void ReenterAfter<T>(Task<T> target, Func<Task<T>, Task> action) => _context.ReenterAfter(target, action);
-
-            public void ReenterAfter(Task target, Action action) => _context.ReenterAfter(target, action);
         }
 
         public static Cluster WithPidCacheInvalidation(this Cluster cluster)
