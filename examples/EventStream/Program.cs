2c4fdec65d0b0cd1042f1a37219477b07d5bea5e
rogerjohansson <rogeralsing@gmail.com>
2021-02-04 10:54:27 +0100

    add eventstream basic example

---
+++ examples/EventStream/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Proto;
+
+namespace EventStream
+{
+    //define some form of message
+    public record SomeMessage(string Name);
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = new ActorSystem();
+            
+            //subscribe to the eventstream via type
+            system.EventStream.Subscribe<SomeMessage>(x => Console.WriteLine($"Got message for {x.Name}"));
+            
+            //publish messages onto the eventstream
+            system.EventStream.Publish(new SomeMessage("ProtoActor"));
+            system.EventStream.Publish(new SomeMessage("Asynkron"));
+
+            //this example is local only.
+            //see ClusterEventStream for cluster broadcast onto the eventstream
+            
+            Console.ReadLine();
+        }
+    }
+}
