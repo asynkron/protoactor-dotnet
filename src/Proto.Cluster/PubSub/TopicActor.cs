bc994d9ea0f3af1f8cdf14cc542f928d1702b953
Roger Johansson <roger@asynkron.se>
2021-02-28 12:50:28 +0100

    .

--- src/Proto.Cluster/PubSub/TopicActor.cs
+++ src/Proto.Cluster/PubSub/TopicActor.cs
@@ -11,10 +11,24 @@ namespace Proto.Cluster.PubSub
 {
     public class TopicActor : IActor
     {
-        private ImmutableHashSet<string> _subscribers = ImmutableHashSet<string>.Empty;
-        public Task ReceiveAsync(IContext context)
+        private ImmutableHashSet<SubscriberIdentity> _subscribers = ImmutableHashSet<SubscriberIdentity>.Empty;
+        public Task ReceiveAsync(IContext context) => context.Message switch
         {
-            throw new NotImplementedException();
+            Subscribe sub     => OnSubscribe(context, sub),
+            Unsubscribe unsub => OnUnsubscribe(context, unsub),
+            _                 => Task.CompletedTask,
+        };
+
+        private Task OnUnsubscribe(IContext context, Unsubscribe unsub)
+        {
+            _subscribers = _subscribers.Remove(unsub.Subscriber);
+            return Task.CompletedTask;
+        }
+
+        private Task OnSubscribe(IContext context, Subscribe sub)
+        {
+            _subscribers = _subscribers.Add(sub.Subscriber);
+            return Task.CompletedTask;
         }
     }
 }
