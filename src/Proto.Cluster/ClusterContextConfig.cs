712161ea5e38abf85965b43785618bbab6fca04c
Piotr Błachut <piotr.blachut@skyrise.tech>
2021-03-03 11:59:23 +0100

    remove hardcoded configuration from DefaultClusterContext

---
+++ src/Proto.Cluster/ClusterContextConfig.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ClusterContextConfiguration.cs" company="Asynkron AB">
+//      Copyright (C) 2015-$CURRENT_YEAR$ Asynkron AB All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace Proto.Cluster
+{
+    public record ClusterContextConfig
+    {
+        public TimeSpan ActorRequestTimeout { get; init; }
+        public TimeSpan RequestLogThrottlePeriod { get; init; }
+        public int MaxNumberOfEventsInRequestLogThrottlePeriod { get; init; }
+        public ClusterContextConfig()
+        {
+            ActorRequestTimeout = TimeSpan.FromSeconds(5);
+            MaxNumberOfEventsInRequestLogThrottlePeriod = 3;
+            RequestLogThrottlePeriod = TimeSpan.FromSeconds(2);
+        }
+    }
+
+    public static class ClusterConfigExtensions
+    {
+        public static ClusterContextConfig ToClusterContextConfig(this ClusterConfig clusterConfig)
+            => new()
+            {
+                ActorRequestTimeout = clusterConfig.ActorRequestTimeout,
+                MaxNumberOfEventsInRequestLogThrottlePeriod = clusterConfig.MaxNumberOfEventsInRequestLogThrottlePeriod,
+                RequestLogThrottlePeriod = clusterConfig.RequestLogThrottlePeriod
+            };
+    }
+}
