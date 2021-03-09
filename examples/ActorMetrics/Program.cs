dc3e12cae2b2bc24ce3b6a568d288d2440c868a7
Roger Johansson <roger@asynkron.se>
2021-03-09 19:45:55 +0100

    Metrics example

---
+++ examples/ActorMetrics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Proto;
+using Ubiquitous.Metrics.Labels;
+using Ubiquitous.Metrics.Prometheus;
+
+namespace ActorMetrics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ActorSystemConfig.Setup().WithMetricsProviders(new PrometheusConfigurator());
+            var system = new ActorSystem(config);
+            var props = Props.FromFunc(ctx => {
+                    Console.WriteLine(ctx.Message);
+                    return Task.CompletedTask;
+                }
+            );
+
+            var pid = system.Root.Spawn(props);
+            system.Root.Send(pid, "hello");
+
+            Console.ReadLine();
+        }
+    }
+}
