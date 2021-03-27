f74e7530609db4046a5a14b1a4ff7a12c827c3b4
Roger Johansson <rogeralsing@gmail.com>
2021-03-27 13:37:50 +0100

    just use locks... (#825)

--- src/Proto.Cluster/Member/MemberList.cs
+++ src/Proto.Cluster/Member/MemberList.cs
@@ -7,18 +7,16 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Threading;
 using JetBrains.Annotations;
 using Microsoft.Extensions.Logging;
 using Proto.Cluster.Data;
-using Proto.Cluster.Events;
 using Proto.Remote;
 using Proto.Utils;
 
 namespace Proto.Cluster
 {
     //This class is responsible for figuring out what members are currently active in the cluster
-    //it will receive a list of memberstatuses from the IClusterProvider
+    //it will receive a list of Members from the IClusterProvider
     //from that, we calculate a delta, which members joined, or left.
 
     //TODO: check usage and threadsafety.
@@ -31,8 +29,6 @@ namespace Proto.Cluster
         private uint _currentMembershipHashCode = uint.MinValue;
 
         private readonly IRootContext _root;
-
-        private readonly ReaderWriterLockSlim _rwLock = new();
         private readonly ActorSystem _system;
         private ImmutableDictionary<string, int> _indexByAddress = ImmutableDictionary<string, int>.Empty;
 
@@ -46,7 +42,7 @@ namespace Proto.Cluster
         private ImmutableDictionary<int, Member> _membersByIndex = ImmutableDictionary<int, Member>.Empty;
 
         private ImmutableDictionary<string, IMemberStrategy> _memberStrategyByKind = ImmutableDictionary<string, IMemberStrategy>.Empty;
-
+        private readonly ConcurrentSet<string> _bannedMembers = new();
         private int _nextMemberIndex;
 
         public MemberList(Cluster cluster)
@@ -55,95 +51,30 @@ namespace Proto.Cluster
             _system = _cluster.System;
             _root = _system.Root;
             _eventStream = _system.EventStream;
-
             _logger = Log.CreateLogger($"MemberList-{_cluster.LoggerId}");
-
-            _bannedMembers = new ConcurrentSet<string>();
+            _cluster.System.EventStream.Subscribe<ClusterTopologyNotification>(OnClusterTopologyNotification);
         }
 
-        //TODO: actually use this to prevent banned members from rejoining
-        private ConcurrentSet<string> _bannedMembers { get; init; }
-
-        public bool IsLeader => _cluster.System.Id.Equals(_leader?.MemberId);
+        private void OnClusterTopologyNotification(ClusterTopologyNotification ctn)
+        {
+            _logger.LogDebug("[MemberList] Got ClusterTopologyNotification {ClusterTopologyNotification}", ctn);
+        }
 
         public Member? GetActivator(string kind, string requestSourceAddress)
         {
-            //TODO: clean this lock logic up
-            var locked = _rwLock.TryEnterReadLock(1000);
-
-            while (!locked)
-            {
-                _logger.LogDebug("MemberList did not acquire reader lock within 1 seconds, retry");
-                locked = _rwLock.TryEnterReadLock(1000);
-            }
-
-            try
+            lock (this)
             {
                 if (_memberStrategyByKind.TryGetValue(kind, out var memberStrategy))
                     return memberStrategy.GetActivator(requestSourceAddress);
-                else
-                {
-                    _logger.LogError("MemberList did not find any activator for kind '{Kind}'", kind);
-                    return null;
-                }
+
+                _logger.LogError("MemberList did not find any activator for kind '{Kind}'", kind);
+                return null;
             }
-            finally
-            {
-                _rwLock.ExitReadLock();
-            }
-        }
-
-        //if the new leader is different from the current leader
-        //notify via the event stream
-        //TODO: cluster state update really. not only leader
-        public void UpdateLeader(LeaderInfo leader)
-        {
-            //TODO: could likely be done better
-            if (leader?.BannedMembers is not null)
-            {
-                foreach (var b in leader.BannedMembers)
-                {
-                    _bannedMembers.Add(b);
-                }
-            }
-
-            if (leader?.MemberId == _leader?.MemberId)
-            {
-                //leader is the same as before, ignore
-                //this can happen eg. if you run blocking queries with consul
-                //if nothing changes within the blocking time, you will still get a result,
-                //we will still get this notification.
-                //we use the memberlist to diff the data against current state
-                return;
-            }
-
-            var oldLeader = _leader;
-
-            _leader = leader;
-
-            _logger.LogInformation("Leader updated {Leader}", leader?.MemberId);
-            _eventStream.Publish(new LeaderElectedEvent(leader, oldLeader));
-
-            if (IsLeader)
-                _logger.LogInformation("I am leader!");
-            else
-                _logger.LogInformation("{Address}:{Id} is leader!", leader?.Address, leader?.MemberId);
         }
 
         public void UpdateClusterTopology(IReadOnlyCollection<Member> statuses, ulong eventId)
         {
-            
-            
-            
-            var locked = _rwLock.TryEnterWriteLock(1000);
-
-            while (!locked)
-            {
-                _logger.LogDebug("MemberList did not acquire writer lock within 1 seconds, retry");
-                locked = _rwLock.TryEnterWriteLock(1000);
-            }
-
-            try
+            lock (this)
             {
                 _logger.LogDebug("Updating Cluster Topology");
                 var topology = new ClusterTopology {EventId = eventId};
@@ -158,7 +89,7 @@ namespace Proto.Cluster
                     statuses
                         .Where(s => !_bannedMembers.Contains(s.Id))
                         .ToArray();
-                
+
                 var newMembershipHashCode = Member.GetMembershipHashCode(nonBannedStatuses);
 
                 //same topology, bail out
@@ -226,59 +157,64 @@ namespace Proto.Cluster
                 if (topology.Left.Count > 0) _logger.LogInformation("Cluster members left {MembersJoined}", topology.Left);
 
                 _eventStream.Publish(topology);
-            }
-            finally
-            {
-                _rwLock.ExitWriteLock();
-            }
-        }
 
-        private void MemberLeave(Member memberThatLeft)
-        {
-            //update MemberStrategy
-            foreach (var k in memberThatLeft.Kinds)
-            {
-                if (!_memberStrategyByKind.TryGetValue(k, out var ms)) continue;
-
-                ms.RemoveMember(memberThatLeft);
-
-                if (ms.GetAllMembers().Count == 0) _memberStrategyByKind.Remove(k);
+                //Notify other members...
+                BroadcastEvent(new ClusterTopologyNotification
+                    {
+                        MemberId = _cluster.System.Id,
+                        MembershipHashCode = _currentMembershipHashCode
+                    }, false
+                );
             }
 
-            _bannedMembers.Add(memberThatLeft.Id);
 
-            _members = _members.Remove(memberThatLeft.Id);
-            _membersByIndex = _membersByIndex.Remove(memberThatLeft.Index);
-
-            if (_indexByAddress.TryGetValue(memberThatLeft.Address, out var member) && memberThatLeft.Id.Equals(member))
-                _indexByAddress = _indexByAddress.Remove(memberThatLeft.Address);
-
-            var endpointTerminated = new EndpointTerminatedEvent {Address = memberThatLeft.Address};
-            _logger.LogDebug("Published event {@EndpointTerminated}", endpointTerminated);
-            _cluster.System.EventStream.Publish(endpointTerminated);
-        }
-
-        private void MemberJoin(Member newMember)
-        {
-            newMember.Index = _nextMemberIndex++;
-
-            //TODO: looks fishy, no locks, are we sure this is safe? it is using private state _vars
-
-            _members = _members.Add(newMember.Id, newMember);
-            _membersByIndex = _membersByIndex.Add(newMember.Index, newMember);
-            _indexByAddress = _indexByAddress.Add(newMember.Address, newMember.Index);
-
-            foreach (var kind in newMember.Kinds)
+            void MemberLeave(Member memberThatLeft)
             {
-                if (!_memberStrategyByKind.ContainsKey(kind))
+                //update MemberStrategy
+                foreach (var k in memberThatLeft.Kinds)
                 {
-                    _memberStrategyByKind = _memberStrategyByKind.SetItem(kind,
-                        _cluster.Config!.MemberStrategyBuilder(_cluster, kind) ?? new SimpleMemberStrategy()
-                    );
+                    if (!_memberStrategyByKind.TryGetValue(k, out var ms)) continue;
+
+                    ms.RemoveMember(memberThatLeft);
+
+                    if (ms.GetAllMembers().Count == 0)
+                    {
+                        _memberStrategyByKind = _memberStrategyByKind.Remove(k);
+                    }
                 }
 
-                //TODO: this doesnt work, just use the same strategy for all kinds...
-                _memberStrategyByKind[kind].AddMember(newMember);
+                _bannedMembers.Add(memberThatLeft.Id);
+
+                _members = _members.Remove(memberThatLeft.Id);
+                _membersByIndex = _membersByIndex.Remove(memberThatLeft.Index);
+
+                if (_indexByAddress.TryGetValue(memberThatLeft.Address, out _))
+                    _indexByAddress = _indexByAddress.Remove(memberThatLeft.Address);
+
+                var endpointTerminated = new EndpointTerminatedEvent {Address = memberThatLeft.Address};
+                _logger.LogDebug("Published event {@EndpointTerminated}", endpointTerminated);
+                _cluster.System.EventStream.Publish(endpointTerminated);
+            }
+
+            void MemberJoin(Member newMember)
+            {
+                newMember.Index = _nextMemberIndex++;
+                
+                _members = _members.Add(newMember.Id, newMember);
+                _membersByIndex = _membersByIndex.Add(newMember.Index, newMember);
+                _indexByAddress = _indexByAddress.Add(newMember.Address, newMember.Index);
+
+                foreach (var kind in newMember.Kinds)
+                {
+                    if (!_memberStrategyByKind.ContainsKey(kind))
+                    {
+                        _memberStrategyByKind = _memberStrategyByKind.SetItem(kind,
+                            _cluster.Config!.MemberStrategyBuilder(_cluster, kind) ?? new SimpleMemberStrategy()
+                        );
+                    }
+
+                    _memberStrategyByKind[kind].AddMember(newMember);
+                }
             }
         }
 
@@ -286,6 +222,7 @@ namespace Proto.Cluster
         ///     broadcast a message to all members eventstream
         /// </summary>
         /// <param name="message"></param>
+        /// <param name="includeSelf"></param>
         public void BroadcastEvent(object message, bool includeSelf = true)
         {
             foreach (var (id, member) in _members.ToArray())
