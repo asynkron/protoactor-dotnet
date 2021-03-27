6cf5814b2b14ce2ad0b111d342459bef353e4414
Magne Helleborg <magne.helleborg@gmail.com>
2021-03-27 15:35:22 +0100

    Cleanup

--- src/Proto.Cluster/ClusterConfig.cs
+++ src/Proto.Cluster/ClusterConfig.cs
@@ -4,7 +4,6 @@
 // </copyright>
 // -----------------------------------------------------------------------
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using JetBrains.Annotations;
