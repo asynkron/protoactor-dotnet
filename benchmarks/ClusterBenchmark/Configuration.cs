8784c54e3fdc5aa50170c9bdccd9f0deb4a70d48
Roger Johansson <roger@asynkron.se>
2021-03-31 10:11:51 +0200

    in memory identity lookup for profiling

--- benchmarks/ClusterBenchmark/Configuration.cs
+++ benchmarks/ClusterBenchmark/Configuration.cs
@@ -85,7 +85,7 @@ namespace ClusterExperiment1
             }
         }
 
-        public static IIdentityLookup GetIdentityLookup() =>  new PartitionIdentityLookup(TimeSpan.FromSeconds(1), TimeSpan.FromMilliseconds(500));
+        public static IIdentityLookup GetIdentityLookup() => new InMemIdentityLookup();//   new PartitionIdentityLookup(TimeSpan.FromSeconds(1), TimeSpan.FromMilliseconds(500));
 
         private static IIdentityLookup GetRedisIdentityLookup()
         {
