e7893a1c4c4868f5be5111ddeef6d360c98a864c
Roger Johansson <roger@asynkron.se>
2021-04-12 12:37:17 +0200

    minor fixes

--- src/Proto.Actor/Utils/EmptyKeyValueStore.cs
+++ src/Proto.Actor/Utils/EmptyKeyValueStore.cs
@@ -5,7 +5,6 @@
 // -----------------------------------------------------------------------
 using System.Threading;
 using System.Threading.Tasks;
-using Proto.Utils.Proto.Utils;
 
 namespace Proto.Utils
 {
