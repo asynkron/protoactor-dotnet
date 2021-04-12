30dc3d406c822477b310613c8faea3c29b028765
Roger Johansson <roger@asynkron.se>
2021-04-12 11:15:50 +0200

    func subscriber

--- src/Proto.Cluster/PubSub/PubSubExtensions.cs
+++ src/Proto.Cluster/PubSub/PubSubExtensions.cs
@@ -6,14 +6,17 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Proto.Remote;
 
 namespace Proto.Cluster.PubSub
 {
+    [PublicAPI]
     public static class PubSubExtensions
     {
         public static Producer Producer(this Cluster cluster,string topic) => new(cluster,topic);
 
+        // Subscribe Cluster Identity
         public static async Task Subscribe(this Cluster cluster,  string topic, string subscriberIdentity, string subscriberKind) => _ = await cluster.RequestAsync<SubscribeResponse>(topic, "topic", new SubscribeRequest()
             {
                 Subscriber = new SubscriberIdentity
@@ -27,6 +30,7 @@ namespace Proto.Cluster.PubSub
             }, CancellationToken.None
         );
 
+        //Subscribe PID
         public static async Task Subscribe(this Cluster cluster,  string topic, PID subscriber) => _ = await cluster.RequestAsync<SubscribeResponse>(topic, "topic", new SubscribeRequest
             {
                 Subscriber = new SubscriberIdentity
@@ -35,5 +39,13 @@ namespace Proto.Cluster.PubSub
                 }
             }, CancellationToken.None
         );
+
+        //Subscribe Receive function, ad-hoc actor
+        public static Task Subscribe(this Cluster cluster, string topic, Receive receive)
+        {
+            var props = Props.FromFunc(receive);
+            var pid = cluster.System.Root.Spawn(props);
+            return cluster.Subscribe(topic, pid);
+        } 
     }
 }
