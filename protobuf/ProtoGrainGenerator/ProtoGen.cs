f9b0d7c6bbcaa9a93f28d28812e046a82763e8e4
Roger Johansson <roger@asynkron.se>
2021-04-17 02:07:02 +0200

    .

--- protobuf/ProtoGrainGenerator/ProtoGen.cs
+++ protobuf/ProtoGrainGenerator/ProtoGen.cs
@@ -15,31 +15,31 @@ namespace MSBuildTasks
         public override bool Execute()
         {
             Log.LogMessage(MessageImportance.High, "Running ProtoGen!!!!");
-            EmbeddedAssembly.Load("ProtoGrainGenerator.deps.Handlebars.dll","Handlebars.dll");
-            EmbeddedAssembly.Load("ProtoGrainGenerator.deps.protobuf-net.dll","protobuf-net.dll");
-            EmbeddedAssembly.Load("ProtoGrainGenerator.deps.protobuf-net.Core.dll","protobuf-net.Core.dll");
-            EmbeddedAssembly.Load("ProtoGrainGenerator.deps.protobuf-net.Reflection.dll","protobuf-net.Reflection.dll");
-            
-            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
-            
-            var currentProject = BuildEngine.ProjectFileOfTaskNode;
-            var dir = Path.GetDirectoryName(currentProject)!;
-            var protoFiles = Directory.GetFiles(dir, "*.proto", new EnumerationOptions
-                {
-                    RecurseSubdirectories = true
-                }
-            )!;
-
-            foreach (var protoFile in protoFiles)
-            {
-                Log.LogMessage(MessageImportance.High, $"Protofile! {protoFile}");
-                var protoDir = Path.GetDirectoryName(protoFile);
-                var outputFile = Path.Combine(protoDir!, protoFile + ".cs");
-
-                var fiIn = new FileInfo(protoFile);
-                var fiOut = new FileInfo(outputFile);
-                Generator.GenerateOne(fiIn, fiOut, Array.Empty<DirectoryInfo>());
-            }
+            // EmbeddedAssembly.Load("ProtoGrainGenerator.deps.Handlebars.dll","Handlebars.dll");
+            // EmbeddedAssembly.Load("ProtoGrainGenerator.deps.protobuf-net.dll","protobuf-net.dll");
+            // EmbeddedAssembly.Load("ProtoGrainGenerator.deps.protobuf-net.Core.dll","protobuf-net.Core.dll");
+            // EmbeddedAssembly.Load("ProtoGrainGenerator.deps.protobuf-net.Reflection.dll","protobuf-net.Reflection.dll");
+            //
+            // AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+            //
+            // var currentProject = BuildEngine.ProjectFileOfTaskNode;
+            // var dir = Path.GetDirectoryName(currentProject)!;
+            // var protoFiles = Directory.GetFiles(dir, "*.proto", new EnumerationOptions
+            //     {
+            //         RecurseSubdirectories = true
+            //     }
+            // )!;
+            //
+            // foreach (var protoFile in protoFiles)
+            // {
+            //     Log.LogMessage(MessageImportance.High, $"Protofile! {protoFile}");
+            //     var protoDir = Path.GetDirectoryName(protoFile);
+            //     var outputFile = Path.Combine(protoDir!, protoFile + ".cs");
+            //
+            //     var fiIn = new FileInfo(protoFile);
+            //     var fiOut = new FileInfo(outputFile);
+            //     Generator.GenerateOne(fiIn, fiOut, Array.Empty<DirectoryInfo>());
+            // }
 
             return true;
         }
