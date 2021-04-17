90742e6a4f41145c9e418b27f7325091af6b0a25
Roger Johansson <roger@asynkron.se>
2021-04-17 04:44:36 +0200

    fix template bug

--- protobuf/ProtoGrainGenerator/Template.cs
+++ protobuf/ProtoGrainGenerator/Template.cs
@@ -66,7 +66,7 @@ namespace {{CsNamespace}}
             return res switch
             {
                 // normal response
-                GrainResponseMessage grainResponse => (HelloResponse)grainResponse.ResponseMessage,
+                GrainResponseMessage grainResponse => ({{OutputName}})grainResponse.ResponseMessage,
                 // error response
                 GrainErrorResponse grainErrorResponse => throw new Exception(grainErrorResponse.Err),
                 // unsupported response
