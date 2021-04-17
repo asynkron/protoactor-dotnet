d0d396e7fbe80293c888b23dd5de9952438c1a91
Roger Johansson <roger@asynkron.se>
2021-04-17 20:50:49 +0200

    .

--- protobuf/ProtoGrainGenerator/ProtoGen.cs
+++ protobuf/ProtoGrainGenerator/ProtoGen.cs
@@ -10,23 +10,12 @@ namespace MSBuildTasks
     public class ProtoGen : Task
     {
 
+        public string BaseIntermediateOutputPath { get; set; }
+        
         public override bool Execute()
         {
-            // This will get the current WORKING directory (i.e. \bin\Debug)
-            var path1 = Environment.CurrentDirectory;
-            // or: Directory.GetCurrentDirectory() gives the same result
-            
-            // This will get the current PROJECT bin directory (ie ../bin/)
-            var path2 = Directory.GetParent(path1).Parent.FullName;
-            
-            // This will get the current PROJECT directory
-            var path3 = Directory.GetParent(path1!)!.Parent!.Parent!.FullName;
-            
-            Log.LogMessage(MessageImportance.High, "Path1 " + path1);
-            Log.LogMessage(MessageImportance.High, "Path2 " + path2);
-            Log.LogMessage(MessageImportance.High, "Path3 " + path3);
-            
-            
+            Log.LogMessage("Intermediate OutputPath: " + BaseIntermediateOutputPath);
+
             Log.LogMessage(MessageImportance.High, "Running Proto.GrainGenerator");
 
             var projectFile = BuildEngine.ProjectFileOfTaskNode;
