540bae7613aadede7e7197fd4f2643b3b80212f5
Roger Johansson <roger@asynkron.se>
2021-05-03 20:51:12 +0200

    cluster benchmark fix

--- benchmarks/ClusterBenchmark/Program.cs
+++ benchmarks/ClusterBenchmark/Program.cs
@@ -207,32 +207,39 @@ namespace ClusterExperiment1
             _ = SafeTask.Run(async () => {
                     var cluster = await Configuration.SpawnClient();
                     var rnd = new Random();
-
+                    var semaphore = new AsyncSemaphore(5);
+                    
                     while (true)
                     {
-                        var requests = new List<Task>();
-
-                        try
-                        {
-                            
-                            var ct = CancellationTokens.FromSeconds(20);
-                            for (var i = 0; i < batchSize; i++)
-                            {
-                                var id = "myactor" + rnd.Next(0, actorCount);
-                                var request = SendRequest(cluster, id, ct);
-
-                                requests.Add(request);
-                            }
-
-                            await Task.WhenAll(requests);
-                        }
-                        catch (Exception x)
-                        {
-                            logger.LogError(x, "Error...");
-                        }
+                        semaphore.Wait(() => RunBatch(rnd, cluster) );
                     }
                 }
             );
+
+            async Task RunBatch(Random? rnd, Cluster cluster)
+            {
+
+                var requests = new List<Task>();
+
+                try
+                {
+                    var ct = CancellationTokens.FromSeconds(20);
+
+                    for (var i = 0; i < batchSize; i++)
+                    {
+                        var id = "myactor" + rnd.Next(0, actorCount);
+                        var request = SendRequest(cluster, id, ct);
+
+                        requests.Add(request);
+                    }
+
+                    await Task.WhenAll(requests);
+                }
+                catch (Exception x)
+                {
+                    logger.LogError(x, "Error...");
+                }
+            }
         }
 
         private static void RunClient()
