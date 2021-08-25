4da2c4917caee823035484e57cf41e3cb942b823
Roger Johansson <roger@asynkron.se>
2021-08-25 11:23:29 +0200

    Add error logging to grain request serialization (#1147)
    Fix null grain requests

--- src/Proto.Cluster/Grain/GrainRequest.cs
+++ src/Proto.Cluster/Grain/GrainRequest.cs
@@ -3,20 +3,28 @@
 //      Copyright (C) 2015-2021 Asynkron AB All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
+using System;
 using Google.Protobuf;
+using Microsoft.Extensions.Logging;
 using Proto.Remote;
 
 namespace Proto.Cluster
 {
     public partial class GrainRequest : IRootSerialized
     {
+        private static readonly ILogger Logger = Log.CreateLogger<GrainRequest>();
         //deserialize into the in-process message type that the grain actors understands
         public IRootSerializable Deserialize(ActorSystem system)
         {
-            if (MessageData.IsEmpty) return new GrainRequestMessage(MethodIndex, null);
+            //special case for null messages
+            if (MessageData.IsEmpty && string.IsNullOrEmpty(MessageTypeName))
+            {
+                return new GrainRequestMessage(MethodIndex, null);
+            }
 
             var ser = system.Serialization();
             var message = ser.Deserialize(MessageTypeName, MessageData, Serialization.SERIALIZER_ID_PROTOBUF);
+            
             return new GrainRequestMessage(MethodIndex, (IMessage) message);
         }
     }
