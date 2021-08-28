b7878b57b9a62b8a61f85893cab705a0f4075038
Roger Johansson <roger@asynkron.se>
2021-08-28 18:52:27 +0200

    make members await self (#1149)

--- examples/EventStreamTopicsCluster/Program.cs
+++ examples/EventStreamTopicsCluster/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualBasic;
 using Microsoft.VisualBasic.CompilerServices;
 using Proto;
@@ -8,11 +9,14 @@ using Proto.Cluster.Consul;
 using Proto.Cluster.Partition;
 using Proto.Remote;
 using Proto.Remote.GrpcCore;
-using ProtosReflection = Proto.ProtosReflection;
+
 
 namespace EventStreamTopicsCluster
 {
-    public record SomeMessage(string Name, string Topic) : ITopicMessage;
+    public partial class MyMessage : ITopicMessage
+    {
+        
+    }
 
     public interface ITopicMessage
     {
@@ -23,12 +27,16 @@ namespace EventStreamTopicsCluster
     {
         private static async Task Main()
         {
+            //Configure ProtoActor to use Serilog
+            var l = LoggerFactory.Create(x => x.AddConsole().SetMinimumLevel(LogLevel.Information));
+            Log.SetLoggerFactory(l);
+            
             var remoteConfig = GrpcCoreRemoteConfig
                 .BindToLocalhost()
-                .WithProtoMessages(ProtosReflection.Descriptor);
+                .WithProtoMessages(MessagesReflection.Descriptor);
 
             var consulProvider =
-                new ConsulProvider(new ConsulProviderConfig(), c => c.Address = new Uri("http://consul:8500/"));
+                new ConsulProvider(new ConsulProviderConfig());
 
             var clusterConfig =
                 ClusterConfig
@@ -42,24 +50,38 @@ namespace EventStreamTopicsCluster
                 .Cluster()
                 .StartMemberAsync();
 
+            Console.WriteLine("Started");
+
             //subscribe to the eventstream via type, just like you do locally
-            system.EventStream.SubscribeToTopic<SomeMessage>("MyTopic.*", x => Console.WriteLine($"Got message for {x.Name}"));
+            system.EventStream.SubscribeToTopic<MyMessage>("MyTopic.*", x => Console.WriteLine($"Got message for {x.Name}"));
 
             //publish messages onto the eventstream on Subtopic1 on MyTopic root
             //but do this using cluster broadcasting. this will publish the event
             //to the event stream on all the members in the cluster
             //this is best effort only, if some member is unavailable, there is no guarantee associated here
-            system.Cluster().BroadcastEvent(new SomeMessage("ProtoActor", "MyTopic.Subtopic1"));
+            system.Cluster().BroadcastEvent(new MyMessage {
+                Name = "ProtoActor", 
+                Topic = "MyTopic.Subtopic1"
+            });
 
             //this message is published on a topic that is not subscribed to, and nothing will happen
-            system.Cluster().BroadcastEvent(new SomeMessage("Asynkron", "AnotherTopic"));
+            system.Cluster().BroadcastEvent(new MyMessage {
+                Name = "Asynkron", 
+                Topic = "AnotherTopic"
+            });
 
             //send a message to the same root topic, but another child topic
-            system.Cluster().BroadcastEvent(new SomeMessage("Do we get this?", "MyTopic.Subtopic1"));
+            system.Cluster().BroadcastEvent(new MyMessage
+                {
+                    Name = "Do we get this?",
+                    Topic = "MyTopic.Subtopic1"
+                }
+            );
 
             //this example is local only.
             //see ClusterEventStream for cluster broadcast onto the eventstream
 
+            Console.WriteLine("Done");
             Console.ReadLine();
         }
     }
