61e3b8db2af86824de2c59c2c842da791f01f11c
Roger Johansson <rogeralsing@gmail.com>
2021-10-16 11:25:14 +0200

    obsolete grpc.core

---
+++ src/Proto.Remote/GrpcNet/GrpcNetChannelProvider.cs
@@ -0,0 +1,21 @@
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace Proto.Remote.GrpcNet
+{
+    public class GrpcNetChannelProvider : IChannelProvider
+    {
+        private readonly GrpcNetRemoteConfig _remoteConfig;
+
+        public GrpcNetChannelProvider(GrpcNetRemoteConfig remoteConfig) => _remoteConfig = remoteConfig;
+
+        public ChannelBase GetChannel(string address)
+        {
+            var addressWithProtocol =
+                $"{(_remoteConfig.UseHttps ? "https://" : "http://")}{address}";
+
+            var channel = GrpcChannel.ForAddress(addressWithProtocol, _remoteConfig?.ChannelOptions ?? new GrpcChannelOptions());
+            return channel;
+        }
+    }
+}
