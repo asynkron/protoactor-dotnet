d9ea0193d970c366f04ab6d918fa0d63ba569700
Roger Johansson <rogeralsing@gmail.com>
2021-10-30 17:54:00 +0200

    add cluster identity overload

--- src/Proto.Cluster/ClusterExtension.cs
+++ src/Proto.Cluster/ClusterExtension.cs
@@ -37,6 +37,13 @@ namespace Proto.Cluster
             var task = context.System.Cluster().RequestAsync<T>(identity, kind, message, context, ct);
             context.ReenterAfter(task, callback);
         }
+        
+        public static void ClusterRequestReenter<T>(this IContext context, ClusterIdentity clusterIdentity, object message, Func<Task<T>, Task> callback, CancellationToken ct)
+        {
+            //call cluster RequestReenter using actor context
+            var task = context.System.Cluster().RequestAsync<T>(clusterIdentity, message, context, ct);
+            context.ReenterAfter(task, callback);
+        }
 
         public static Props WithClusterIdentity(this Props props, ClusterIdentity clusterIdentity)
             => props.WithOnInit(context => context.Set(clusterIdentity));
