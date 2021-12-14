d2975545859a04dbb1b8e7d307399c190d5c39ec
Roger Johansson <roger.johansson@charge-amps.com>
2021-12-14 12:10:13 +0100

    add settings

--- src/Proto.Actor/Logging/ActorLoggingContext.cs
+++ src/Proto.Actor/Logging/ActorLoggingContext.cs
@@ -13,45 +13,56 @@ namespace Proto
     [PublicAPI]
     public static class ActorLoggingContextExtensions
     {
-        public static Props WithLoggingContextDecorator(this Props props, ILogger logger, bool ignoreInfrastructureMessages = true) => 
-            props.WithContextDecorator(ctx => new ActorLoggingContext(ctx, logger, ignoreInfrastructureMessages));
+        public static Props WithLoggingContextDecorator(
+            this Props props,
+            ILogger logger,
+            LogLevel logLevel = LogLevel.Debug,
+            LogLevel infrastructureLogLevel = LogLevel.None
+        ) =>
+            props.WithContextDecorator(ctx => new ActorLoggingContext(ctx, logger, logLevel, infrastructureLogLevel));
     }
 
     public class ActorLoggingContext : ActorContextDecorator
     {
         private readonly ILogger _logger;
-        private readonly bool _ignoreInfrastructureMessages;
+        private readonly LogLevel _logLevel;
+        private readonly LogLevel _infrastructureLogLevel;
 
-        public ActorLoggingContext(IContext context, ILogger logger, bool ignoreInfrastructureMessages = true) : base(context)
+        public ActorLoggingContext(
+            IContext context,
+            ILogger logger,
+            LogLevel logLevel = LogLevel.Debug,
+            LogLevel infrastructureLogLevel = LogLevel.None
+        ) : base(context)
         {
             _logger = logger;
-            _ignoreInfrastructureMessages = ignoreInfrastructureMessages;
+            _logLevel = logLevel;
+            _infrastructureLogLevel = infrastructureLogLevel;
         }
 
         public override async Task Receive(MessageEnvelope envelope)
         {
             var message = envelope.Message;
 
-            //ignore any built in messages
-            if (_ignoreInfrastructureMessages && message is InfrastructureMessage)
-            {
-                await base.Receive(envelope);
-                return;
-            }
-
-            _logger.LogInformation("Actor {Self} {ActorType} received message {Message}", Self, ActorType, message);
+            var logLevel = GetLogLevel(message);
+            _logger.Log(logLevel, "Actor {Self} {ActorType} received message {Message}", Self, ActorType, message);
 
             try
             {
                 await base.Receive(envelope);
-                _logger.LogInformation("Actor {Self} {ActorType} completed message {Message}", Self, ActorType, message);
+                _logger.Log(logLevel, "Actor {Self} {ActorType} completed message {Message}", Self, ActorType, message);
             }
             catch (Exception x)
             {
-                _logger.LogInformation(x, "Actor {Self} {ActorType} failed during message {Message}", Self, ActorType, message);
-                
+                _logger.LogError(x, "Actor {Self} {ActorType} failed during message {Message}", Self, ActorType, message);
                 throw;
             }
+        }
+
+        private LogLevel GetLogLevel(object message)
+        {
+            var logLevel = message is InfrastructureMessage ? _infrastructureLogLevel : _logLevel;
+            return logLevel;
         }
 
         public override PID SpawnNamed(Props props, string name)
@@ -71,18 +82,11 @@ namespace Proto
 
         public override void Respond(object message)
         {
-            //ignore any built in messages
-            if (_ignoreInfrastructureMessages && message is InfrastructureMessage)
-            {
-                base.Respond(message);
-                return;
-            }
-
-            _logger.LogInformation("Actor {Self} {ActorType} responded with {Message} to {Sender}", Self, ActorType, message, Sender);
+            var logLevel = GetLogLevel(message);
+            _logger.Log(logLevel, "Actor {Self} {ActorType} responded with {Message} to {Sender}", Self, ActorType, message, Sender);
             base.Respond(message);
         }
 
         private string ActorType => Actor?.GetType().Name;
-
     }
 }
