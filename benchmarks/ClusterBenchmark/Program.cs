ff9a522d0bb5761b9e1e661409ce6bc464e9363a
Magne Helleborg <magne.helleborg@gmail.com>
2022-01-02 21:37:48 +0100

    Added traceability in Cluster Benchmark

--- benchmarks/ClusterBenchmark/Program.cs
+++ benchmarks/ClusterBenchmark/Program.cs
@@ -377,7 +377,7 @@ namespace ClusterExperiment1
                 followers.Add(p);
             }
 
-            await Task.Delay(15000);
+            await Task.Delay(4000);
 
             startClient();
             Console.WriteLine("Client started...");
