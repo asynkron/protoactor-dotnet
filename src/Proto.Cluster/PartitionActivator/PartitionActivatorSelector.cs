2ab865b5f7023067ca9eda6613c84276d588c49c
Roger Johansson <rogeralsing@gmail.com>
2022-01-14 21:18:46 +0100

    Make partition activator work with latest changes

---
+++ src/Proto.Cluster/PartitionActivator/PartitionActivatorSelector.cs
@@ -0,0 +1,33 @@
+// -----------------------------------------------------------------------
+// <copyright file="PartitionMemberSelector.cs" company="Asynkron AB">
+//      Copyright (C) 2015-2020 Asynkron AB All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+using System;
+using System.Collections.Immutable;
+
+namespace Proto.Cluster.Partition
+{
+    //this class is responsible for translating between Identity->activator member
+    //this is the key algorithm for the distributed hash table
+    class PartitionActivatorSelector
+    {
+        private readonly object _lock = new();
+        private MemberHashRing _rdv = new(ImmutableList<Member>.Empty);
+        private ulong _topologyHash;
+
+        public void Update(Member[] members, ulong topologyHash)
+        {
+            lock (_lock)
+            {
+                _rdv = new MemberHashRing(members);
+                _topologyHash = topologyHash;
+            }
+        }
+
+        public string GetOwner(ClusterIdentity key)
+        {
+            lock (_lock) return _rdv.GetOwnerMemberByIdentity(key);
+        }
+    }
+}
