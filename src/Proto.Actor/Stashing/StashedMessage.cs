8ece729065e99698b96f2065f5b0da13485e81b0
Roger Johansson <roger.johansson@charge-amps.com>
2022-01-19 13:38:54 +0100

    .

---
+++ src/Proto.Actor/Stashing/StashedMessage.cs
@@ -0,0 +1,9 @@
+// -----------------------------------------------------------------------
+// <copyright file="StashedMessage.cs" company="Asynkron AB">
+//      Copyright (C) 2015-2022 Asynkron AB All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Proto.Stashing
+{
+    public record StashedMessage();
+}
