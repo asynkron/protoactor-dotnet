33e189c5123a93ee02d742264c49dbde82303fd1
Roger Johansson <rogeralsing@gmail.com>
2022-01-30 21:06:14 +0100

    add partition activator tests

--- src/Proto.Cluster/Seed/SeedNodeClusterProvider.cs
+++ src/Proto.Cluster/Seed/SeedNodeClusterProvider.cs
@@ -3,25 +3,16 @@
 //      Copyright (C) 2015-2022 Asynkron AB All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
-using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
-using Proto.Cluster.Gossip;
 
 namespace Proto.Cluster.Seed
 {
     public class SeedNodeClusterProvider : IClusterProvider
     {
-        private readonly TimeSpan _heartbeatExpiration;
         private readonly CancellationTokenSource _cts = new();
         private PID? _pid;
         private Cluster? _cluster;
-        private static readonly ILogger Logger = Log.CreateLogger<SeedNodeClusterProvider>(); 
-
-        public SeedNodeClusterProvider(TimeSpan? heartbeatExpiration=null) => 
-            _heartbeatExpiration = heartbeatExpiration ?? TimeSpan.FromSeconds(5);
 
         public Task StartMemberAsync(Cluster cluster)
         {
