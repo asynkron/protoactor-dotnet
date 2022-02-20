c080f8a4f62ee5d71d58a24f49502dfdd2bf97bd
Roger Johansson <rogeralsing@gmail.com>
2022-02-20 22:27:04 +0100

    do not error log if safe task was cancelled (#1472)

--- src/Proto.Actor/Utils/TaskFactory.cs
+++ src/Proto.Actor/Utils/TaskFactory.cs
@@ -17,9 +17,15 @@ namespace Proto
 
         public static async Task Run(Func<Task> body, CancellationToken cancellationToken = default, [CallerMemberName] string name = "")
         {
+            Task? t = null;
             try
             {
-                await Task.Run(body, cancellationToken);
+                t = Task.Run(body, cancellationToken);
+                await t;
+            }
+            catch (TaskCanceledException e) when (e.Task == t)
+            {
+                //pass. do not log if our own task was cancelled
             }
             catch (Exception x)
             {
