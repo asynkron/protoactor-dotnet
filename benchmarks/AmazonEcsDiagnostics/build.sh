c785df98bc50f4e832e9913795730b074989287f
Roger Johansson <rogeralsing@gmail.com>
2022-03-04 20:06:50 +0100

    remove non used files

--- benchmarks/AmazonEcsDiagnostics/build.sh
+++
@@ -1,4 +0,0 @@
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/m4u6b5p7
-docker build . -t prototest
-docker tag prototest:latest public.ecr.aws/m4u6b5p7/prototest:latest
-docker push public.ecr.aws/m4u6b5p7/prototest:latest
