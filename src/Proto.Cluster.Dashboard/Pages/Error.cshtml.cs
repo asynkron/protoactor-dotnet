96cf8ec868d8f3b5f220d6a362c0448c0c812235
Roger Johansson <rogeralsing@gmail.com>
2022-03-12 20:34:54 +0100

    dashboard cleanup

--- src/Proto.Cluster.Dashboard/Pages/Error.cshtml.cs
+++ src/Proto.Cluster.Dashboard/Pages/Error.cshtml.cs
@@ -1,27 +1,19 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace Proto.Cluster.Dashboard.Pages;
 
-[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-[IgnoreAntiforgeryToken]
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true), IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
     private readonly ILogger<ErrorModel> _logger;
 
-    public ErrorModel(ILogger<ErrorModel> logger)
-    {
-        _logger = logger;
-    }
+    public ErrorModel(ILogger<ErrorModel> logger) => _logger = logger;
 
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-    public void OnGet()
-    {
-        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-    }
+    public void OnGet() => RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
 }
