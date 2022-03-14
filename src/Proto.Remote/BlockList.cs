9eaea0db657025f79c40194102451028f6540303
Roger Johansson <rogeralsing@gmail.com>
2022-03-14 20:03:42 +0100

    blocked members ttl to prevent block list to grow forever

--- src/Proto.Remote/BlockList.cs
+++ src/Proto.Remote/BlockList.cs
@@ -3,9 +3,10 @@
 //      Copyright (C) 2015-2022 Asynkron AB All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using Proto.Utils;
+using System.Linq;
 
 namespace Proto.Remote;
 
@@ -14,19 +15,25 @@ public class BlockList
     private readonly object _lock = new();
 
     public ImmutableHashSet<string> BlockedMembers { get; private set; } = ImmutableHashSet<string>.Empty;
-    public void Block(string memberId)
-    {
-        lock (_lock)
-        {
-            BlockedMembers = BlockedMembers.Add(memberId);
-        }
-    }
+
+    private ImmutableDictionary<string, DateTime> _blockedMembers  = ImmutableDictionary<string,DateTime>.Empty;
 
     public void Block(IEnumerable<string> memberIds)
     {
         lock (_lock)
         {
-            BlockedMembers = BlockedMembers.Union(memberIds);
+            foreach (var member in memberIds)
+            {
+                if (!_blockedMembers.ContainsKey(member))
+                {
+                    _blockedMembers = _blockedMembers.Add(member, DateTime.UtcNow);
+                }
+            }
+            
+            BlockedMembers = _blockedMembers
+                .Where(kvp => kvp.Value > DateTime.UtcNow.AddHours(-1))
+                .Select(kvp => kvp.Key)
+                .ToImmutableHashSet();
         }
     }
 
