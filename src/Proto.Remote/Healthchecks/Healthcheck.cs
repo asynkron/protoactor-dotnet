4600dfb9250ecad819da5f1de8be82c586c5ff65
Roger Johansson <roger@asynkron.se>
2022-05-17 22:18:40 +0200

    .

--- src/Proto.Remote/Healthchecks/Healthcheck.cs
+++ src/Proto.Remote/Healthchecks/Healthcheck.cs
@@ -22,7 +22,7 @@ public class ActorSystemHealthCheck : IHealthCheck
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new()) =>
         _system.Shutdown.IsCancellationRequested switch
         {
-            true => Task.FromResult(HealthCheckResult.Unhealthy()),
+            true => Task.FromResult(HealthCheckResult.Unhealthy("ActorSystem has been stopped")),
             _    => Task.FromResult(HealthCheckResult.Healthy())
         };
 }
