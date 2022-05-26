b53959bd27c1481dda05c8425318fc6acdee746f
Roger Johansson <roger@asynkron.se>
2022-05-26 15:39:18 +0200

    handle termination

--- src/Proto.Cluster/Sharding/ShardActor.cs
+++ src/Proto.Cluster/Sharding/ShardActor.cs
@@ -3,32 +3,45 @@
 //      Copyright (C) 2015-2022 Asynkron AB All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Proto.Cluster.Sharding;
 
 
-public delegate Props ShardEntityProducer(string shardId, string entityId, IContext parentContext); 
+public delegate Props ShardEntityProducer(string shardId, string entityId, IContext parentContext);
+
 public class ShardActor : IActor
 {
     private ShardEntityProducer _propsFactory;
-    private readonly Dictionary<string, PID> _children;
+    private readonly Dictionary<string, PID> _childrenLookup;
+    private readonly Dictionary<string, string> _childrenReverseLookup;
     
     //TODO: handle restarts, stop children, restart children etc.
     public ShardActor(ShardEntityProducer propsFactory)
     {
         _propsFactory = propsFactory;
-        _children = new();
+        _childrenLookup = new();
+        _childrenReverseLookup = new();
     }
 
     public Task ReceiveAsync(IContext context)
     {
-        if (context.Message is IShardMessage sm)
+        return context.Message switch
         {
-            return OnShardMessage(sm, context);
-        }
+            IShardMessage sm => OnShardMessage(sm, context),
+            Terminated t     => OnChildTerminated(t, context),
+            _                => Task.CompletedTask
+        };
+    }
+
+    private Task OnChildTerminated(Terminated terminated, IContext context)
+    {
+        if (!_childrenReverseLookup.TryGetValue(terminated.Who.Id, out var key)) 
+            return Task.CompletedTask;
+
+        _childrenLookup.Remove(key);
+        _childrenReverseLookup.Remove(terminated.Who.Id);
 
         return Task.CompletedTask;
     }
@@ -42,12 +55,14 @@ public class ShardActor : IActor
 
     private PID EnsureEntityExists(string shardId, string entityId, IContext context)
     {
-        if (_children.TryGetValue(entityId, out var pid))
+        if (_childrenLookup.TryGetValue(entityId, out var pid))
             return pid;
 
         var props = _propsFactory(shardId, entityId, context);
         pid = context.SpawnNamed(props, entityId);
-        _children.Add(entityId, pid);
+        
+        _childrenLookup.Add(entityId, pid);
+        _childrenReverseLookup.Add(pid.Id, entityId);
 
         return pid;
     }
