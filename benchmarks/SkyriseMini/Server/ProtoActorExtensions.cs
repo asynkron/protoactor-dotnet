4fe12f59e8387ac8c93e57dc087446e353c03e74
Roger Johansson <roger@asynkron.se>
2022-07-01 16:23:41 +0200

    .

--- benchmarks/SkyriseMini/Server/ProtoActorExtensions.cs
+++ benchmarks/SkyriseMini/Server/ProtoActorExtensions.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.IO.Compression;
+using Grpc.Net.Client;
+using Grpc.Net.Compression;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Proto;
 using Proto.Cluster;
 using Proto.Cluster.Consul;
+using Proto.Cluster.Partition;
 using Proto.Cluster.PartitionActivator;
 using Proto.DependencyInjection;
 using Proto.Remote;
@@ -25,6 +29,7 @@ public static class ProtoActorExtensions
 
             var actorSystemConfig = ActorSystemConfig
                 .Setup()
+                .WithSharedFutures()
                 .WithDeadLetterThrottleCount(3)
                 .WithDeadLetterThrottleInterval(TimeSpan.FromSeconds(1));
 
@@ -32,12 +37,20 @@ public static class ProtoActorExtensions
 
             var remoteConfig = GrpcNetRemoteConfig.BindToLocalhost()
                 .WithProtoMessages(ProtoActorSut.Contracts.ProtosReflection.Descriptor)
+                .WithChannelOptions(new GrpcChannelOptions
+                    {
+                        CompressionProviders = new[]
+                        {
+                            new GzipCompressionProvider(CompressionLevel.Fastest)
+                        }
+                    }
+                )
                 .WithLogLevelForDeserializationErrors(LogLevel.Critical);
 
             var clusterProvider = new ConsulProvider(new ConsulProviderConfig());
             
             var clusterConfig = ClusterConfig
-                .Setup(config["ClusterName"], clusterProvider, new PartitionActivatorLookup())
+                .Setup(config["ClusterName"], clusterProvider, new PartitionIdentityLookup())
                 .WithClusterKind("PingPongRaw",Props.FromProducer(() => new PingPongActorRaw()) );
             
             system
