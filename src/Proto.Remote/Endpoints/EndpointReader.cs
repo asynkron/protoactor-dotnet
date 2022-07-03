c1ce0ca06072734885f9667e0ca8665545757707
Roger Johansson <roger@asynkron.se>
2022-07-03 22:37:30 +0200

    .

--- src/Proto.Remote/Endpoints/EndpointReader.cs
+++ src/Proto.Remote/Endpoints/EndpointReader.cs
@@ -175,7 +175,9 @@ public sealed class EndpointReader : Remoting.RemotingBase
             {
                 var currentMessage = requestStream.Current;
                 if (_endpointManager.CancellationToken.IsCancellationRequested)
+                {
                     continue;
+                }
 
                 _endpointManager.RemoteMessageHandler.HandleRemoteMessage(currentMessage, address!);
             }
