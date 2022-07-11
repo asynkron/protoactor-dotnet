4491caa5932bbffd58634d5ad0fb4dbb70f27425
Marcin Budny <marcin.budny@etteplan.com>
2022-07-11 13:15:50 +0200

    is the problem caused by cancel token expiring too late?

--- src/Proto.Cluster/PubSub/PubSubMemberDeliveryActor.cs
+++ src/Proto.Cluster/PubSub/PubSubMemberDeliveryActor.cs
@@ -68,7 +68,7 @@ public class PubSubMemberDeliveryActor : IActor
             // deliver to virtual actor
             // delivery should always be possible, since a virtual actor always exists
             var response = await context.ClusterRequestAsync<PublishResponse?>(ci.Identity, ci.Kind, pub,
-                CancellationTokens.WithTimeout(_subscriberTimeout), _subscriberTimeout
+                CancellationTokens.WithTimeout(TimeSpan.FromMilliseconds(_subscriberTimeout.TotalMilliseconds / 2)), _subscriberTimeout
             );
 
             if (response == null)
