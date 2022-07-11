c5becfa48fbb9bdb545f372c71fe2487a3f41000
Marcin Budny <marcin.budny@etteplan.com>
2022-07-11 10:05:48 +0200

    does disabling parallelism fix CI?

---
+++ tests/Proto.Cluster.PubSub.Tests/NoParallelTests.cs
@@ -0,0 +1,8 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file = "NoParallelTests.cs" company = "Asynkron AB">
+//      Copyright (C) 2015-2022 Asynkron AB All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
