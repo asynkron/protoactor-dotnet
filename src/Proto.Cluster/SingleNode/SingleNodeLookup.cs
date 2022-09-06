11321e75067ab91ccfacd2c3057729f3c38d7b6b
Magne Helleborg <magne.helleborg@gmail.com>
2022-09-06 22:58:23 +0200

    .

--- src/Proto.Cluster/SingleNode/SingleNodeLookup.cs
+++ src/Proto.Cluster/SingleNode/SingleNodeLookup.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="SingleNodeActivatorLookup.cs" company="Asynkron AB">
+// <copyright file="SingleNodeLookup.cs" company="Asynkron AB">
 //      Copyright (C) 2015-2022 Asynkron AB All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
