6c27bc8d021d5355c4bdcb8348f83cbd3d3ba17a
michalnarwojsz <115669594+michalnarwojsz@users.noreply.github.com>
2022-10-17 09:03:59 +0200

    Retry request on DeadLetterResponse (#1822)
    Instead of failing virtual actor request when DeadLetterResponse is received, retry the request.
    
    Also make sure that DeadLetterResponse is not returned to client when request is called with object as type parameter.
    
    Fixes #1799

---
+++ tests/Proto.Cluster.Tests/RetryOnDeadLetterTests.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ClusterTest.Messages;
+using FluentAssertions;
+using Xunit;
+
+namespace Proto.Cluster.Tests;
+
+public class RetryOnDeadLetterTests
+{
+    [Fact]
+    public async Task ShouldRetryRequestOnDeadLetterResponseRegardlessOfResponseType()
+    {
+        await using var fixture = new Fixture(1);
+        await fixture.InitializeAsync();
+
+        var member = fixture.Members.First();
+        var identity = CreateIdentity("dead-letter-test");
+
+        // make sure the actor is created and the PID is cached
+        await member.RequestAsync<Pong>(identity, EchoActor.Kind, new Ping(), CancellationTokens.FromSeconds(1));
+
+        // pretend we have an invalid PID in the cache
+        var otherMember = await fixture.SpawnNode();
+        if (member.PidCache.TryGet(ClusterIdentity.Create(identity, EchoActor.Kind), out var pid))
+        {
+            var newPid = PID.FromAddress(otherMember.System.Address, pid.Id);
+            if (!member.PidCache.TryUpdate(ClusterIdentity.Create(identity, EchoActor.Kind), newPid, pid))
+            {
+                Assert.Fail("Failed to replace actor's pid with a fake one in the pid cache");
+            }
+        }
+        else
+        {
+            Assert.Fail("Did not find expected actor identity in the pid cache");
+        }
+
+        // check if the correct response type is returned
+        var response = await member.RequestAsync<object>(identity, EchoActor.Kind, new Ping(), CancellationTokens.FromSeconds(1));
+        response.Should().BeOfType<Pong>();
+
+    }
+
+    private string CreateIdentity(string baseId) => $"{Guid.NewGuid().ToString("N").Substring(0, 6)}-{baseId}-";
+    
+    private class Fixture : BaseInMemoryClusterFixture
+    {
+        public Fixture(int clusterSize)
+            : base(clusterSize, cc => cc
+                .WithActorRequestTimeout(TimeSpan.FromSeconds(1))
+            )
+        {
+        }
+    }
+}
