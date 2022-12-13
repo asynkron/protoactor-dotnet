9538ff779f42487b8b074a33df067931a9d57f78
Gürkan Güran <gurkanguran@gurkanguransMBP.domain_not_set.invalid>
2022-12-13 15:36:56 +0100

    Implemented Proto Cluster for Azure Container Apps

---
+++ Proto.Cluster.AzureContainerApps/ArmClientUtils.cs
@@ -0,0 +1,82 @@
+using Azure;
+using Azure.Core;
+using Azure.ResourceManager;
+using Azure.ResourceManager.AppContainers;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Resources.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Proto.Cluster.AzureContainerApps;
+
+public static class ArmClientUtils
+{
+    private static readonly ILogger Logger = Log.CreateLogger(nameof(ArmClientUtils));
+
+    public static async Task<Member[]> GetClusterMembers(this ArmClient client, ResourceIdentifier resourceGroup, string containerAppName)
+    {
+        var members = new List<Member>();
+        
+        var containerApp = await client.GetResourceGroupResource(resourceGroup).GetContainerAppAsync(containerAppName);
+
+        if (containerApp is null || !containerApp.HasValue)
+        {
+            Logger.LogError("Container App: {ContainerApp} in resource group: {ResourceGroup} is not found", containerApp, resourceGroup);
+            return members.ToArray();
+        }
+
+        var containerAppRevisions = GetActiveRevisionsWithTraffic(containerApp);
+        
+        Logger.LogError("Container App: {ContainerApp} in resource group: {ResourceGroup} does not contain any active revisions with traffic", containerAppName, resourceGroup);
+        
+        foreach (var revision in containerAppRevisions)
+        {
+            foreach (var replica in revision.GetContainerAppReplicas())
+            {
+                var tags = replica.GetTagResource().Data.TagValues;
+
+                if (!tags.ContainsKey(ProtoLabels.LabelMemberId))
+                {
+                    Logger.LogWarning("Skipping Replica {Id}, no Proto Tags found", replica.Data.Id);
+                    continue;
+                }
+
+                var kinds = tags
+                    .Where(kvp => kvp.Key.StartsWith(ProtoLabels.LabelKind))
+                    .Select(kvp => kvp.Key[(ProtoLabels.LabelKind.Length + 1)..])
+                    .ToArray();
+
+                var member = new Member
+                {
+                    Id = tags[ProtoLabels.LabelMemberId],
+                    Port = int.Parse(tags[ProtoLabels.LabelPort]),
+                    Host = revision.Data.Template.Containers.First().Probes.First().HttpRequest.Host,
+                    Kinds = { kinds }
+                };
+
+                members.Add(member);
+            }
+        }
+
+        return members.ToArray();
+    }
+
+    public static async Task UpdateMemberMetadata(this ArmClient client, ResourceIdentifier resourceGroup, string containerAppName, string revisionName, string replicaName, Dictionary<string, string> tags)
+    {
+        var resourceTag = new Tag();
+        foreach (var tag in tags)
+        {
+            resourceTag.TagValues.Add(tag);
+        }
+
+        var containerApp = await client.GetResourceGroupResource(resourceGroup).GetContainerAppAsync(containerAppName);
+        var revision = await containerApp.Value.GetContainerAppRevisionAsync(revisionName);
+        var replica = await revision.Value.GetContainerAppReplicaAsync(replicaName);
+        await replica.Value.GetTagResource().CreateOrUpdateAsync(WaitUntil.Completed, new TagResourceData(resourceTag));
+    }
+    
+    private static IEnumerable<ContainerAppRevisionResource> GetActiveRevisionsWithTraffic(ContainerAppResource containerApp)
+    {
+        return containerApp.GetContainerAppRevisions()
+            .Where(r => r.HasData && r.Data.Active.GetValueOrDefault(false) && r.Data.TrafficWeight > 0);
+    }
+}
---
+++ Proto.Cluster.AzureContainerApps/AzureContainerAppsProvider.cs
@@ -0,0 +1,168 @@
+﻿using Azure.Core;
+using Azure.ResourceManager;
+using Azure.ResourceManager.AppContainers;
+using Microsoft.Extensions.Logging;
+using Proto.Utils;
+
+namespace Proto.Cluster.AzureContainerApps;
+
+public class AzureContainerAppsProvider  : IClusterProvider
+{
+    private readonly ArmClient _client;
+    private readonly string _resourceGroup;
+    private readonly string _containerAppName;
+    private readonly string _revisionName;
+    private readonly string _replicaName;
+
+    private string _address = null!;
+    private Cluster _cluster = null!;
+    private string _clusterName = null!;
+    private string[] _kinds = null!;
+    private int _port;
+    
+    private static readonly ILogger Logger = Log.CreateLogger<AzureContainerAppsProvider>();
+    private static readonly TimeSpan PollIntervalInSeconds = TimeSpan.FromSeconds(5);
+
+    public AzureContainerAppsProvider(
+        ArmClient client,
+        string resourceGroup, 
+        string containerAppName,
+        string revisionName,
+        string replicaName)
+    {
+        _client = client;
+        _resourceGroup = resourceGroup;
+        _containerAppName = containerAppName;
+        _revisionName = revisionName;
+        _replicaName = replicaName;
+    }
+
+    public async Task StartMemberAsync(Cluster cluster)
+    {
+        var clusterName = cluster.Config.ClusterName;
+        var (host, port) = cluster.System.GetAddress();
+        var kinds = cluster.GetClusterKinds();
+        _cluster = cluster;
+        _clusterName = clusterName;
+        _port = port;
+        _kinds = kinds;
+        _address = $"{host}:{port}";
+        
+        StartClusterMonitor();
+        await RegisterMemberAsync();
+    }
+
+    public Task StartClientAsync(Cluster cluster)
+    {
+        var clusterName = cluster.Config.ClusterName;
+        var (host, port) = cluster.System.GetAddress();
+        _cluster = cluster;
+        _clusterName = clusterName;
+        _port = port;
+        _kinds = Array.Empty<string>();
+        
+        StartClusterMonitor();
+        return Task.CompletedTask;
+    }
+
+    public async Task ShutdownAsync(bool graceful) => await DeregisterMemberAsync();
+    
+    private async Task RegisterMemberAsync()
+    {
+        await Retry.Try(RegisterMemberInner, onError: OnError, onFailed: OnFailed, retryCount: Retry.Forever);
+
+        static void OnError(int attempt, Exception exception) =>
+            Logger.LogWarning(exception, "Failed to register service");
+
+        static void OnFailed(Exception exception) => Logger.LogError(exception, "Failed to register service");
+    }
+
+    private async Task RegisterMemberInner()
+    {
+        var containerApp = await _client.GetResourceGroupResource(new ResourceIdentifier(_resourceGroup)).GetContainerAppAsync(_containerAppName);
+        var revision = await containerApp.Value.GetContainerAppRevisionAsync(_revisionName);
+
+        if (revision.Value.Data.TrafficWeight.GetValueOrDefault(0) == 0)
+        {
+            return;
+        }
+        
+        Logger.LogInformation(
+            "[Cluster][AzureContainerAppsProvider] Registering service {ReplicaName} on {IpAddress}", 
+            _replicaName,
+            _address);
+
+        var tags = new Dictionary<string, string>
+        {
+            [ProtoLabels.LabelCluster] = _clusterName,
+            [ProtoLabels.LabelPort] = _port.ToString(),
+            [ProtoLabels.LabelMemberId] = _cluster.System.Id
+        };
+
+        foreach (var kind in _kinds)
+        {
+            var labelKey = $"{ProtoLabels.LabelKind}-{kind}";
+            tags.TryAdd(labelKey, "true");
+        }
+
+        try
+        {
+            await _client.UpdateMemberMetadata(new ResourceIdentifier(_resourceGroup), _containerAppName, _revisionName, _replicaName, tags);
+        }
+        catch (Exception x)
+        {
+            Logger.LogError(x, "Failed to update metadata");
+        }
+    }
+
+    private void StartClusterMonitor() =>
+        _ = SafeTask.Run(async () =>
+            {
+                while (!_cluster.System.Shutdown.IsCancellationRequested)
+                {
+                    Logger.LogInformation("Calling ECS API");
+
+                    try
+                    {
+                        var members = await _client.GetClusterMembers(new ResourceIdentifier(_resourceGroup), _containerAppName);
+
+                        if (members.Any())
+                        {
+                            Logger.LogInformation("Got members {Members}", members.Length);
+                            _cluster.MemberList.UpdateClusterTopology(members);
+                        }
+                        else
+                        {
+                            Logger.LogWarning("Failed to get members from Azure Container Apps");
+                        }
+                    }
+                    catch (Exception x)
+                    {
+                        Logger.LogError(x, "Failed to get members from Azure Container Apps");
+                    }
+
+                    await Task.Delay(PollIntervalInSeconds);
+                }
+            }
+        );
+
+    private async Task DeregisterMemberAsync()
+    {
+        await Retry.Try(DeregisterMemberInner, onError: OnError, onFailed: OnFailed);
+
+        static void OnError(int attempt, Exception exception) =>
+            Logger.LogWarning(exception, "Failed to deregister service");
+
+        static void OnFailed(Exception exception) => Logger.LogError(exception, "Failed to deregister service");
+    }
+
+    private async Task DeregisterMemberInner()
+    {
+        Logger.LogInformation(
+            "[Cluster][AzureContainerAppsProvider] Unregistering member {ReplicaName} on {IpAddress}", 
+            _replicaName,
+            _address);
+
+        await _client.UpdateMemberMetadata(new ResourceIdentifier(_resourceGroup), _containerAppName, _revisionName, _replicaName,new Dictionary<string, string>());
+    }
+}
