3001da9c91d55e5f2db60b01c3fcc48ef763b1df
woksin <sindrewilting@gmail.com>
2023-02-22 16:57:05 +0100

    Unify logic for WaitUntil and also dispose the CancellationTokenSource

--- tests/Proto.Cluster.PubSub.Tests/WaitHelper.cs
+++ tests/Proto.Cluster.PubSub.Tests/WaitHelper.cs
@@ -8,32 +8,15 @@ namespace Proto.Cluster.PubSub.Tests;
 
 public static class WaitHelper
 {
-    public static async Task WaitUntil(Func<bool> condition, string? errorMessage = null, TimeSpan? timeout = null)
-    {
-        timeout ??= TimeSpan.FromSeconds(15);
-
-        var cts = new CancellationTokenSource(timeout.Value);
-
-        while (!cts.Token.IsCancellationRequested)
-        {
-            if (condition())
-            {
-                return;
-            }
-
-            // ReSharper disable once MethodSupportsCancellation
-            await Task.Delay(100);
-        }
-
-        throw new Exception(errorMessage ?? $"The condition was not met within the timeout of {timeout.Value}");
-    }
+    public static Task WaitUntil(Func<bool> condition, string? errorMessage = null, TimeSpan? timeout = null)
+        => WaitUntil(() => Task.FromResult(condition()), errorMessage, timeout);
 
     public static async Task WaitUntil(Func<Task<bool>> condition, string? errorMessage = null,
         TimeSpan? timeout = null)
     {
         timeout ??= TimeSpan.FromSeconds(15);
 
-        var cts = new CancellationTokenSource(timeout.Value);
+        using var cts = new CancellationTokenSource(timeout.Value);
 
         while (!cts.Token.IsCancellationRequested)
         {
