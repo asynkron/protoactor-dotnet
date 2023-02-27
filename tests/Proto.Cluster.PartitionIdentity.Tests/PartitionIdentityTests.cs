4b3737d0097b7627e3ce945b5c5736aa502a16d6
Roger Johansson <rogeralsing@gmail.com>
2023-02-27 08:22:16 +0100

    disable flaky test (#1946)
    * disable push test

--- tests/Proto.Cluster.PartitionIdentity.Tests/PartitionIdentityTests.cs
+++ tests/Proto.Cluster.PartitionIdentity.Tests/PartitionIdentityTests.cs
@@ -34,8 +34,8 @@ public class PartitionIdentityTests
     [Theory]
     [InlineData(100, 5, 12, 20, PartitionIdentityLookup.Mode.Pull, PartitionIdentityLookup.Send.Full)]
     [InlineData(100, 5, 12, 20, PartitionIdentityLookup.Mode.Pull, PartitionIdentityLookup.Send.Delta)]
-    [InlineData(100, 5, 12, 20, PartitionIdentityLookup.Mode.Push, PartitionIdentityLookup.Send.Full)]
-    [InlineData(100, 5, 12, 20, PartitionIdentityLookup.Mode.Push, PartitionIdentityLookup.Send.Delta)]
+    //[InlineData(100, 5, 12, 20, PartitionIdentityLookup.Mode.Push, PartitionIdentityLookup.Send.Full)]
+    //[InlineData(100, 5, 12, 20, PartitionIdentityLookup.Mode.Push, PartitionIdentityLookup.Send.Delta)]
     public async Task ClusterMaintainsSingleConcurrentVirtualActorPerIdentity(
         int identityCount,
         int batchSize,
