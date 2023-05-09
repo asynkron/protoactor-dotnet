d67fb14023db6779168ad91b7c149860711a1d9d
Roger Johansson <roger@asynkron.se>
2023-05-09 13:50:32 +0200

    update example

--- examples/AspNetGrains/Node2/Program.cs
+++ examples/AspNetGrains/Node2/Program.cs
@@ -2,19 +2,25 @@ using AspNetGrains.Messages;
 using Proto;
 using Proto.Cluster;
 using Proto.Cluster.Seed;
+using Proto.Cluster.SeedNode.Redis;
 using Proto.Remote;
 using Proto.Remote.HealthChecks;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddLogging(x => x.AddConsole());
 
+var multiplexer = ConnectionMultiplexer.Connect("localhost:6379");
+var discovery = new RedisSeedNodeDiscovery(multiplexer);
+
 builder.Services.AddProtoCluster("MyCluster", port: 0,
     configureRemote: r => r.WithProtoMessages(AspNetGrains.Messages.ProtosReflection.Descriptor),
-    configureCluster: c => c.WithClusterKind(HelloGrainActor.GetClusterKind((ctx,ci) => new Node2.HelloGrain(ctx,ci.Identity))), 
-    clusterProvider:SeedNodeClusterProvider.JoinSeedNode("localhost",8090));
+    configureCluster: c =>
+        c.WithClusterKind(HelloGrainActor.GetClusterKind((ctx, ci) => new Node2.HelloGrain(ctx, ci.Identity))),
+    clusterProvider: SeedNodeClusterProvider.JoinWithDiscovery(discovery));
 
-builder.Services.AddHealthChecks().AddCheck<ActorSystemHealthCheck>("proto", null, new[] { "ready", "live" });
+builder.Services.AddHealthChecks().AddCheck<ClusterHealthCheck>("proto", null, new[] { "ready", "live" });
 
 var app = builder.Build();
 
---
+++ src/Proto.Cluster.SeedNode.Redis/RedisSeedNodeDiscovery.cs
@@ -0,0 +1,48 @@
+﻿using JetBrains.Annotations;
+using Proto.Cluster.Seed;
+using StackExchange.Redis;
+
+namespace Proto.Cluster.SeedNode.Redis;
+
+[PublicAPI]
+public class RedisSeedNodeDiscovery : ISeedNodeDiscovery
+{
+    private readonly string _storageKey;
+    private readonly IDatabase _db;
+
+    public RedisSeedNodeDiscovery(IConnectionMultiplexer multiplexer, string storageKey = "RedisSeedNode")
+    {
+        _storageKey = storageKey;
+        _db = multiplexer.GetDatabase();
+    }
+    public async Task Register(string memberId, string host, int port)
+    {
+        var entry = new HashEntry(memberId, $"{host}:{port}");
+        await _db.HashSetAsync(Key(), new []{entry} );
+    }
+
+    public async Task Remove(string memberId)
+    {
+        await _db.HashDeleteAsync(Key(), memberId);
+    }
+
+    public async Task<(string memberId, string host, int port)[]> GetAll()
+    {
+        var entries = await _db.HashGetAllAsync(Key());
+        var result = entries.Select(x =>
+        {
+            var parts = x.Value.ToString().Split(':');
+            var memberId = x.Name.ToString();
+            var host = parts[0];
+            var port = int.Parse(parts[1]);
+            return (memberId, host, port);
+        }).ToArray();
+
+        return result;
+    }
+
+    private string Key()
+    {
+        return _storageKey;
+    }
+}
