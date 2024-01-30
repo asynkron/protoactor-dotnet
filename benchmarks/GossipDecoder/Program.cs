69092cfd08e98a929e0b7a6e553d58d21b1c7265
Roger Johansson <roger@asynkron.se>
2024-01-30 19:17:02 +0100

    fix decoder

--- benchmarks/GossipDecoder/Program.cs
+++ benchmarks/GossipDecoder/Program.cs
@@ -6,6 +6,6 @@ using Google.Protobuf.WellKnownTypes;
 using Proto.Cluster;
 
 Console.WriteLine("Hello, World!");
-var data2 = """abc"""; //enter base64 encoded data here
+var data2 = "abc"; //enter base64 encoded data here
 var message = ClusterTopology.Parser.ParseFrom(ByteString.FromBase64(data2));
 Console.WriteLine(message);
