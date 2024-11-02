df4591b7c888f6413926c8f4b1f287e0aaa75b39
benbenwilde <benbenwilde@gmail.com>
2024-11-01 22:30:23 -0500

    add explanation and dockerfile

--- benchmarks/EndpointManagerTest/Program.cs
+++ benchmarks/EndpointManagerTest/Program.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
 using Proto;
 using Proto.Remote;
 using Proto.Remote.GrpcNet;
 
 namespace EndpointManagerTest;
 
+// This program tests lockup issues with the EndpointManager.
+// TL:DR; This is to demonstrate the issue with the locking and blocking waits in EndpointManager, and to confirm the fix.
+//
+// This recreates a scenario we were seeing in our production environments. 
+// What we saw was 30 cluster clients were sending many messages to 2 of the cluster members, who were sending messages to eachother depending
+// on actor placement. If something happens and the 2 members had to reboot, they would end up locking up, not being able to do anything.
+// This scenario has been recreated more simply here, where you have 2 members sending many messages back and forth, a disconnect comes through
+// from a member that recently restarted, and new connections are being opened to other members. Putting all these together, we end up in a situation
+// where many threads get stuck at a lock in EndpointManager, while the one thread inside of the lock is waiting for a ServerConnector to stop.
+// NOTE: that this can be a bit flakey as we are trying to reproduce a complete thread lockup. So there is a dockerfile to run it in a more consistent
+// environment. Using `--cpus="1"` with docker will make it even more consistent, but sometimes it takes a few tries to repro. 
+// You will know you reproduced it when you stop seeing "This should log every second." every second. you may also see the built in
+// "ThreadPool is running hot" log, but the absence of that log is ambiguous, since if it's locked up it won't finish to log how long it took!
+// The other indicator is that all the new connections made at the end should be logging terminations and reconnects and quickly give up (since they don't exist),
+// but of course that won't be happening when you're locked up. Also seeing any "terminating" messages without a corresponding "terminated" message
+// also indicates that you're locked up.
 class Program
 {
 	private static async Task Main()
@@ -16,12 +33,30 @@ class Program
 						.AddFilter("Microsoft", LogLevel.None)
 						.AddFilter("Grpc", LogLevel.None)
 						.AddFilter("Proto.Context.ActorContext", LogLevel.Information)
+						.AddFilter("Proto.Diagnostics.DiagnosticsStore", LogLevel.Warning)
 						.AddFilter("Proto.Remote.ServerConnector", LogLevel.Error)
 						.AddSimpleConsole(o => o.SingleLine = true)
 			)
 		);
 		
 		var logger = Log.CreateLogger("Main");
+		
+		_ = Task.Factory.StartNew(async () =>
+		{
+			while (true)
+			{
+				try
+				{
+					await Task.Factory.StartNew(async () => { await Task.Yield(); });
+				}
+				catch (Exception)
+				{
+				}
+
+				logger.LogInformation("This should log every second [pending: {pendingWorkItems}].", ThreadPool.PendingWorkItemCount);
+				await Task.Delay(1000);
+			}
+		});
 		
 		var sys1 = new ActorSystem().WithRemote(GrpcNetRemoteConfig.BindTo("localhost", 12000).WithRemoteKind("noop", Props.FromProducer(() => new NoopActor())));
 		await sys1.Remote().StartAsync();
@@ -34,7 +69,11 @@ class Program
 		{
 			while (true)
 			{
-				_ = sys1.Root.RequestAsync<Touched>(echoActorOn2, new Touch());
+				for (var i = 0; i < 200; i++)
+				{
+					_ = sys1.Root.RequestAsync<Touched>(echoActorOn2, new Touch());
+				}
+				await Task.Yield();
 			}
 		});
 		
@@ -43,7 +82,11 @@ class Program
 		{
 			while (true)
 			{
-				_ = sys2.Root.RequestAsync<Touched>(echoActorOn1, new Touch());
+				for (var i = 0; i < 200; i++)
+				{
+					_ = sys2.Root.RequestAsync<Touched>(echoActorOn1, new Touch());
+				}
+				await Task.Yield();
 			}
 		});
 		
@@ -52,7 +95,7 @@ class Program
 		sys1.EventStream.Publish(new EndpointTerminatedEvent(false, "localhost:12001", null));
 		
 		var port = 12002;
-		for (var i = 12002; i < 12012; i++)
+		for (var i = 12002; i < 12032; i++)
 		{
 			//logger.LogInformation("Touching {i}", i);
 			_ = sys1.Root.RequestAsync<Touched>(new PID($"localhost:{i}", "$1"), new Touch());
