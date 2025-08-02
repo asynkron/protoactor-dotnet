32f86afda5b08675fb78c251e49e9ee7959c501f
Roger Johansson <roger@asynkron.se>
2025-08-02 18:14:49 +0200

    chore: update copyright headers

--- src/Proto.Cluster/Partition/HandoverSource.cs
+++ src/Proto.Cluster/Partition/HandoverSource.cs
@@ -1,6 +1,6 @@
 ﻿// -----------------------------------------------------------------------
 // <copyright file="HandoverSource.cs" company="Asynkron AB">
-//      Copyright (C) 2015-2024 Asynkron AB All rights reserved
+//      Copyright (C) 2015-2025 Asynkron AB All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
 
--- src/Proto.Cluster/Seed/SeedNodeClusterProvider.cs
+++ src/Proto.Cluster/Seed/SeedNodeClusterProvider.cs
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------
 // <copyright file = "SeedNodeClusterProvider.cs" company = "Asynkron AB">
-//      Copyright (C) 2015-2024 Asynkron AB All rights reserved
+//      Copyright (C) 2015-2025 Asynkron AB All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
 
