b2c2bb17992ec897105884926b0c5a0cee405afe
Roger Johansson <rogeralsing@gmail.com>
2025-08-02 18:15:59 +0200

    chore: update copyright headers (#2184)

--- src/Proto.Actor/Future/FutureBatch.cs
+++ src/Proto.Actor/Future/FutureBatch.cs
@@ -1,6 +1,6 @@
 ﻿// -----------------------------------------------------------------------
 // <copyright file="FutureBatch.cs" company="Asynkron AB">
-//      Copyright (C) 2015-2024 Asynkron AB All rights reserved
+//      Copyright (C) 2015-2025 Asynkron AB All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
 
--- src/Proto.Actor/InternalsVisibleTo.cs
+++ src/Proto.Actor/InternalsVisibleTo.cs
@@ -1,6 +1,6 @@
 ﻿// -----------------------------------------------------------------------
 // <copyright file="InternalsVisibleTo.cs" company="Asynkron AB">
-//      Copyright (C) 2015-2024 Asynkron AB All rights reserved
+//      Copyright (C) 2015-2025 Asynkron AB All rights reserved
 // </copyright>
 // -----------------------------------------------------------------------
 
