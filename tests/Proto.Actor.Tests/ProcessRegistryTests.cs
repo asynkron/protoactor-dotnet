8c2b056f945824de94891897afeb4752698e48b3
Roger Johansson <rogeralsing@gmail.com>
2025-08-03 15:35:18 +0200

    test: expand process registry coverage (#2199)

--- tests/Proto.Actor.Tests/ProcessRegistryTests.cs
+++ tests/Proto.Actor.Tests/ProcessRegistryTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Proto.TestFixtures;
 using Xunit;
@@ -86,5 +87,51 @@ public class ProcessRegistryTests
         var p2 = reg.Get(pid);
 
         Assert.Same(p, p2);
+    }
+
+    [Fact]
+    public async Task Given_ProcessRegistry_NextIdShouldReturnSequentialIds()
+    {
+        var system = new ActorSystem();
+        await using var _ = system;
+        var reg = new ProcessRegistry(system);
+
+        var id1 = reg.NextId();
+        var id2 = reg.NextId();
+
+        Assert.Equal("$1", id1);
+        Assert.Equal("$2", id2);
+    }
+
+    [Fact]
+    public async Task Given_PIDs_FindShouldReturnMatchingPIDs()
+    {
+        var system = new ActorSystem();
+        await using var _ = system;
+        var reg = new ProcessRegistry(system);
+        var p1 = new TestProcess(system);
+        var p2 = new TestProcess(system);
+        var p3 = new TestProcess(system);
+        reg.TryAdd("alpha", p1);
+        reg.TryAdd("beta", p2);
+        reg.TryAdd("ALPHANUM", p3);
+
+        var results = reg.Find("Alp").ToList();
+
+        Assert.Equal(2, results.Count);
+        Assert.All(results, pid => Assert.Equal(system.Address, pid.Address));
+        Assert.Contains(results, pid => pid.Id == "alpha");
+        Assert.Contains(results, pid => pid.Id == "ALPHANUM");
+    }
+
+    [Fact]
+    public async Task Given_RemotePIDWithoutResolver_GetShouldThrow()
+    {
+        var system = new ActorSystem();
+        await using var _ = system;
+        var reg = new ProcessRegistry(system);
+        var pid = PID.FromAddress("remote", "123");
+
+        Assert.Throws<NotSupportedException>(() => reg.Get(pid));
     }
 }
