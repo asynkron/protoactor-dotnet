5831c755908537cccd979770523784984ebd2583
Roger Johansson <roger@asynkron.se>
2025-08-23 10:49:17 +0200

    Add tests for retry cancellation

--- src/Proto.Actor/Utils/Retry.cs
+++ src/Proto.Actor/Utils/Retry.cs
@@ -5,6 +5,7 @@
 // -----------------------------------------------------------------------
 
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Proto.Utils;
@@ -19,75 +20,23 @@ public static class Retry
         int backoffMilliSeconds = 100,
         int maxBackoffMilliseconds = 5000,
         Action<int, Exception>? onError = null,
-        Action<Exception>? onFailed = null
+        Action<Exception>? onFailed = null,
+        CancellationToken ct = default
     ) where T : class =>
         TryUntil(body, res => res != null, retryCount, backoffMilliSeconds, maxBackoffMilliseconds, onError,
-            onFailed);
+            onFailed, ct);
 
-    public static async Task<T> TryUntil<T>(Func<Task<T>> body, Func<T?, bool> condition, int retryCount = 10,
+    public static Task<T> TryUntil<T>(Func<Task<T>> body, Func<T?, bool> condition, int retryCount = 10,
         int backoffMilliSeconds = 100, int maxBackoffMilliseconds = 5000, Action<int, Exception>? onError = null,
-        Action<Exception>? onFailed = null)
-    {
-        for (var i = 0; retryCount == 0 || i < retryCount; i++)
-        {
-            try
-            {
-                var res = await body().ConfigureAwait(false);
+        Action<Exception>? onFailed = null, CancellationToken ct = default) =>
+        TryInternal(body, condition, retryCount, backoffMilliSeconds, maxBackoffMilliseconds, onError, onFailed,
+            ignoreFailure: false, checkFailFast: true, ct);
 
-                if (condition(res))
-                {
-                    return res;
-                }
-            }
-            catch (Exception x)
-            {
-                x.CheckFailFast();
-                onError?.Invoke(i, x);
-
-                if (i == retryCount - 1)
-                {
-                    onFailed?.Invoke(x);
-
-                    throw new RetriesExhaustedException("Retried but failed", x);
-                }
-
-                var backoff = Math.Min(i * backoffMilliSeconds, maxBackoffMilliseconds);
-                await Task.Delay(backoff).ConfigureAwait(false);
-            }
-        }
-
-        throw new RetriesExhaustedException("Retry condition was never met");
-    }
-
-    public static async Task<T> Try<T>(Func<Task<T>> body, int retryCount = 10, int backoffMilliSeconds = 100,
-        int maxBackoffMilliseconds = 5000, Action<int, Exception>? onError = null, Action<Exception>? onFailed = null)
-    {
-        for (var i = 0; retryCount == 0 || i < retryCount; i++)
-        {
-            try
-            {
-                var res = await body().ConfigureAwait(false);
-
-                return res;
-            }
-            catch (Exception x)
-            {
-                onError?.Invoke(i, x);
-
-                if (i == retryCount - 1)
-                {
-                    onFailed?.Invoke(x);
-
-                    throw new RetriesExhaustedException("Retried but failed", x);
-                }
-
-                var backoff = Math.Min(i * backoffMilliSeconds, maxBackoffMilliseconds);
-                await Task.Delay(backoff).ConfigureAwait(false);
-            }
-        }
-
-        throw new RetriesExhaustedException("This should never happen...");
-    }
+    public static Task<T> Try<T>(Func<Task<T>> body, int retryCount = 10, int backoffMilliSeconds = 100,
+        int maxBackoffMilliseconds = 5000, Action<int, Exception>? onError = null, Action<Exception>? onFailed = null,
+        CancellationToken ct = default) =>
+        TryInternal(body, condition: null, retryCount, backoffMilliSeconds, maxBackoffMilliseconds, onError, onFailed,
+            ignoreFailure: false, checkFailFast: false, ct);
 
     public static async Task Try(
         Func<Task> body,
@@ -96,20 +45,61 @@ public static class Retry
         int maxBackoffMilliseconds = 5000,
         Action<int, Exception>? onError = null,
         Action<Exception>? onFailed = null,
-        bool ignoreFailure = false
+        bool ignoreFailure = false,
+        CancellationToken ct = default
+    ) =>
+        await TryInternal(
+                async () =>
+                {
+                    await body().ConfigureAwait(false);
+
+                    return true;
+                },
+                _ => true,
+                retryCount,
+                backoffMilliSeconds,
+                maxBackoffMilliseconds,
+                onError,
+                onFailed,
+                ignoreFailure,
+                checkFailFast: true,
+                ct
+            )
+            .ConfigureAwait(false);
+
+    private static async Task<T> TryInternal<T>(
+        Func<Task<T>> body,
+        Func<T?, bool>? condition,
+        int retryCount,
+        int backoffMilliSeconds,
+        int maxBackoffMilliseconds,
+        Action<int, Exception>? onError,
+        Action<Exception>? onFailed,
+        bool ignoreFailure,
+        bool checkFailFast,
+        CancellationToken ct
     )
     {
         for (var i = 0; retryCount == 0 || i < retryCount; i++)
         {
+            ct.ThrowIfCancellationRequested();
+
             try
             {
-                await body().ConfigureAwait(false);
+                var res = await body().ConfigureAwait(false);
 
-                return;
+                if (condition == null || condition(res))
+                {
+                    return res!;
+                }
             }
             catch (Exception x)
             {
-                x.CheckFailFast();
+                if (checkFailFast)
+                {
+                    x.CheckFailFast();
+                }
+
                 onError?.Invoke(i, x);
 
                 if (i == retryCount - 1)
@@ -118,18 +108,25 @@ public static class Retry
 
                     if (ignoreFailure)
                     {
-                        return;
+                        return default!;
                     }
 
                     throw new RetriesExhaustedException("Retried but failed", x);
                 }
-
-                var backoff = Math.Min(i * backoffMilliSeconds, maxBackoffMilliseconds);
-                await Task.Delay(backoff).ConfigureAwait(false);
             }
+
+            var backoff = Math.Min((i + 1) * backoffMilliSeconds, maxBackoffMilliseconds);
+            // Linear backoff: increase delay per attempt but cap to avoid unbounded waiting
+            await Task.Delay(backoff, ct).ConfigureAwait(false);
         }
 
-        throw new RetriesExhaustedException("This should never happen...");
+        if (ignoreFailure)
+        {
+            return default!;
+        }
+
+        throw new RetriesExhaustedException(
+            condition == null ? "This should never happen..." : "Retry condition was never met");
     }
 
 #pragma warning disable RCS1194
